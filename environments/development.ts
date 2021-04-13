c71ba78a0c4e7043cb98821252324a106e073e98
brucedonovan <bruce.donovan@gmail.com>
2021-04-13 09:40:02 +0100

    timed setup

--- environments/development.ts
+++ environments/development.ts
@@ -16,8 +16,7 @@ import { Ladle } from '../typechain/Ladle'
  */
 
 const { loadFixture } = waffle
-
-const ilksRandom:Uint8Array[] = Array.from({length: 3}, () => ethers.utils.randomBytes(6));
+console.time("Environment deployed in");
 
 const series:Uint8Array[] = Array.from({length: 5}, () => ethers.utils.randomBytes(6));
 const ilks: string[] = ['DAI', 'USDC', 'USDT']
@@ -42,47 +41,42 @@ const fundExternalAccounts = async (assetList:Map<string, any>) => {
 };
 
 /* Update the available series based on Cauldron events */
-const getDeployedSeries = async (cauldronAddress:string): Promise<string[]> => {
-    const cauldron: Cauldron = (await ethers.getContractAt('Cauldron', cauldronAddress ) as unknown) as Cauldron; 
-    /* get both serieAdded events */
-    const seriesAddedEvents = await cauldron.queryFilter('SeriesAdded' as any);
-    /* Get the seriesId */
-    return Promise.all(
-        seriesAddedEvents.map(async (x:any) : Promise<string> => {
-            const { seriesId: id, baseId, fyToken } = cauldron.interface.parseLog(x).args;
-             return fyToken;
-            }
-        )
-    )
-}
+// const getDeployedSeries = async (cauldronAddress:string): Promise<string[]> => {
+//     const cauldron: Cauldron = (await ethers.getContractAt('Cauldron', cauldronAddress ) as unknown) as Cauldron; 
+//     /* get both serieAdded events */
+//     const seriesAddedEvents = await cauldron.queryFilter('SeriesAdded' as any);
+//     /* Get the seriesId */
+//     return Promise.all(
+//         seriesAddedEvents.map(async (x:any) : Promise<string> => {
+//             const { seriesId: id, baseId, fyToken } = cauldron.interface.parseLog(x).args;
+//              return fyToken;
+//             }
+//         )
+//     )
+// }
 
-/* Update the ilks info based on addresses */
-const getIlks = async (ilks:string[])  => {
-
-}
-
-const linkPool = async (pool: Pool, ladleAddress: string) => {
-    const [ ownerAcc ] = await ethers.getSigners();
-    const ladle = await ethers.getContractAt('Ladle', ladleAddress, ownerAcc);
-    const fyToken = (await ethers.getContractAt('FYToken', await pool.fyToken()) as unknown) as FYToken;
-    const seriesId = await fyToken.name()
-    await ladle.addPool(seriesId, pool.address)
-}
+// const linkPool = async (pool: Pool, ladleAddress: string) => {
+//     const [ ownerAcc ] = await ethers.getSigners();
+//     const ladle = await ethers.getContractAt('Ladle', ladleAddress, ownerAcc);
+//     const fyToken = (await ethers.getContractAt('FYToken', await pool.fyToken()) as unknown) as FYToken;
+//     const seriesId = await fyToken.name()
+//     await ladle.addPool(seriesId, pool.address)
+// }
 
 const fixture = async () =>  {
     const [ ownerAcc ] = await ethers.getSigners();
-    
     const yieldEnv = await YieldEnvironment.setup(
         ownerAcc,
         ilks.map((ilk:string) => ethers.utils.isAddress(ilk)? ilk : ethers.utils.formatBytes32String(ilk).slice(0, 14) ),
         series.map((series:Uint8Array ) => ethers.utils.hexlify(series))
     )
-
     return yieldEnv
 }
 
+
 loadFixture(fixture).then( async ( yieldEnv : YieldEnvironment )  => { 
 
+    console.log("--------------- YIELD ENV -------------------")
     console.log(`"Cauldron": "${yieldEnv.cauldron.address}",`)
     console.log(`"Ladle" : "${yieldEnv.ladle.address}",`)
     console.log(`"Witch" : "${yieldEnv.witch.address}"`)
@@ -110,7 +104,10 @@ loadFixture(fixture).then( async ( yieldEnv : YieldEnvironment )  => {
         })
     })
 
-    fundExternalAccounts(yieldEnv.assets);
+    await fundExternalAccounts(yieldEnv.assets);
+
+    console.timeEnd("Environment deployed in")
+    return yieldEnv;
 
 }
 
