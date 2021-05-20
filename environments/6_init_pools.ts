f89e1c6a0711f6c8f3f2782d2201a403f3346160
brucedonovan <bruce.donovan@gmail.com>
2021-05-20 16:42:04 +0100

    bugfix: don't ignore output folder

--- environments/6_init_pools.ts
+++ environments/6_init_pools.ts
@@ -1,25 +1,35 @@
 import { ethers, waffle } from 'hardhat'
+
+import *  as fs from 'fs'
+
+import { jsonToMap, mapToJson } from '../shared/helpers'
 import { WAD } from '../shared/constants'
+
 import { id } from '@yield-protocol/utils-v2'
 
 import { Pool } from '../typechain/Pool'
 import { ERC20Mock } from '../typechain/ERC20Mock'
 import { FYToken } from '../typechain/FYToken'
 
-import { transferFromFunder } from '../shared/helpers';
+import { Series } from '../fixtures/series'
 
-/**
+ /**
+ * This script initiates the pools
  * 
- * README
- * 
- * 
+ * run:
+ * npx hardhat run ./environments/series.ts --network localhost
+ *
  */
+
 const poolAddresses = [
-  ['DAI1Pool', '0x69f9C1D1B2d0F90Cb60C461555267D182b6389ac'],
-  ['DAI2Pool', '0xff52234137A0752fb077e6b3A2838719Ee891874'],
-  ['USDC1Pool', '0xE29694371Bf0149C7745af3A937570aCb29Fc17a'],
-  ['USDC2Pool', '0xBAD1A3e3AD1a22543cd17EE8dbD05fD3c1BC414a'],
+  ['DAI1Pool', '0x019fd0Fc79D57E6c407a9aA4CeDd5C3CAA230DA7'],
+  ['DAI2Pool', '0xD794F6b188055adB36d1f2B8f6342c6e567e773C'],
+  ['USDC1Pool', '0x091C622621e6aef0F11e6Ad260621b14e76FEf8C'],
+  ['USDC2Pool', '0x9cF4D31b1Cf5bAb429239C4C4A1f2095816B8CF2'],
 ]
+
+const json = fs.readFileSync('./output/pools.json', 'utf8')
+const pools = jsonToMap(json) as Series["pools"];
 
  /**
  * 
@@ -31,31 +41,56 @@ const poolAddresses = [
 console.time("Pools initialized");
 
 (async () => {
-    const [ ownerAcc ] = await ethers.getSigners();    
+    const [ ownerAcc ] = await ethers.getSigners(); 
+    pools.forEach(async (pool: Pool)=> {
 
-    for (let [seriesId, poolAddress] of poolAddresses) {
-        const pool = (await ethers.getContractAt('Pool', poolAddress, ownerAcc) as unknown) as Pool
-        const base = (await ethers.getContractAt('ERC20Mock', await pool.base(), ownerAcc) as unknown) as ERC20Mock
-        const fyToken = (await ethers.getContractAt('FYToken', await pool.fyToken(), ownerAcc) as unknown) as FYToken
+      // const connectedPool = pool.connect(ownerAcc)
+      console.log(pool);
 
-        // Supply pool with a million tokens of each for initialization
-        try {
-          // try minting tokens (as the token owner for mock tokens)
-          await base.mint(pool.address, WAD.mul(1000000)); console.log(`base.mint(pool.address)`)
-        } catch (e) { 
-          // if that doesn't work, try transfering tokens from a whale/funder account
-          // await transferFromFunder( base.address, pool.address, WAD.mul(1000000), funder)
-        }
-        // Initialize pool
-        await pool.mint(await ownerAcc.getAddress(), true, 0); console.log(`pool.mint(owner)`)
-    
-        // Donate fyToken to the pool to skew it
-        await fyToken.grantRole(id('mint(address,uint256)'), await ownerAcc.getAddress()); console.log(`fyToken.grantRoles(owner)`)
+      // const base = (await ethers.getContractAt('ERC20Mock', await pool.base(), ownerAcc) as unknown) as ERC20Mock
+      // const fyToken = (await ethers.getContractAt('FYToken', await pool.fyToken(), ownerAcc) as unknown) as FYToken
+      // // Supply pool with a million tokens of each for initialization
+      // try {
+      //   // try minting tokens (as the token owner for mock tokens)
+      //   await base.mint(pool.address, WAD.mul(1000000)); console.log(`base.mint(pool.address)`)
+      // } catch (e) { 
+      //   // if that doesn't work, try transfering tokens from a whale/funder account
+      //   // await transferFromFunder( base.address, pool.address, WAD.mul(1000000), funder)
+      //   console.log(e);
+      // }
+      // // Initialize pool
+      // await pool.mint(await ownerAcc.getAddress(), true, 0); console.log(`pool.mint(owner)`)
   
-        await fyToken.mint(pool.address, WAD.mul(1000000).div(9)); console.log(`fyToken.mint(pool.address)`)
-        await pool.sync(); console.log(`pool.sync`)            
-    }
+      // // Donate fyToken to the pool to skew it
+      // await fyToken.grantRole(id('mint(address,uint256)'), await ownerAcc.getAddress()); console.log(`fyToken.grantRoles(owner)`)
 
+      // await fyToken.mint(pool.address, WAD.mul(1000000).div(9)); console.log(`fyToken.mint(pool.address)`)
+      // await pool.sync(); console.log(`pool.sync`) 
+    })
+
+    // for (let [seriesId, poolAddress] of poolAddrs) {
+    //     const pool = (await ethers.getContractAt('Pool', poolAddress, ownerAcc) as unknown) as Pool
+    //     const base = (await ethers.getContractAt('ERC20Mock', await pool.base(), ownerAcc) as unknown) as ERC20Mock
+    //     const fyToken = (await ethers.getContractAt('FYToken', await pool.fyToken(), ownerAcc) as unknown) as FYToken
+
+    //     // Supply pool with a million tokens of each for initialization
+    //     try {
+    //       // try minting tokens (as the token owner for mock tokens)
+    //       await base.mint(pool.address, WAD.mul(1000000)); console.log(`base.mint(pool.address)`)
+    //     } catch (e) { 
+    //       // if that doesn't work, try transfering tokens from a whale/funder account
+    //       // await transferFromFunder( base.address, pool.address, WAD.mul(1000000), funder)
+    //       console.log(e);
+    //     }
+    //     // Initialize pool
+    //     await pool.mint(await ownerAcc.getAddress(), true, 0); console.log(`pool.mint(owner)`)
     
+    //     // Donate fyToken to the pool to skew it
+    //     await fyToken.grantRole(id('mint(address,uint256)'), await ownerAcc.getAddress()); console.log(`fyToken.grantRoles(owner)`)
+  
+    //     await fyToken.mint(pool.address, WAD.mul(1000000).div(9)); console.log(`fyToken.mint(pool.address)`)
+    //     await pool.sync(); console.log(`pool.sync`)            
+    // }
+
     console.timeEnd("Pools initialized")
 })()
