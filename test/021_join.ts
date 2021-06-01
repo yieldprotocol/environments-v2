6e4d9713e36866d1d0dfd42a969296cfa6021c6c
uivlis <vladsilviufarcas@gmail.com>
2021-06-01 10:28:41 +0100

    Add prettierrc from vault-v2

--- test/021_join.ts
+++ test/021_join.ts
@@ -1,92 +1,75 @@
-import * as fs from "fs";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { id } from "@yield-protocol/utils-v2";
-import { WAD } from "../shared/constants";
+import * as fs from 'fs'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import { id } from '@yield-protocol/utils-v2'
+import { WAD } from '../shared/constants'
 
-import { Join } from "../typechain/Join";
-import { ERC20Mock } from "../typechain/ERC20Mock";
+import { Join } from '../typechain/Join'
+import { ERC20Mock } from '../typechain/ERC20Mock'
 
-import { ethers, waffle } from "hardhat";
-import { expect } from "chai";
-import { jsonToMap } from "../shared/helpers";
+import { ethers, waffle } from 'hardhat'
+import { expect } from 'chai'
+import { jsonToMap } from '../shared/helpers'
 
-import { WBTC } from "../shared/constants";
+import { WBTC } from '../shared/constants'
 
-describe("Join", function () {
-  this.timeout(0);
+describe('Join', function () {
+  this.timeout(0)
 
-  let ownerAcc: SignerWithAddress;
-  let owner: string;
-  let otherAcc: SignerWithAddress;
-  let other: string;
-  let join: Join;
-  let joinFromOther: Join;
-  let token: ERC20Mock;
+  let ownerAcc: SignerWithAddress
+  let owner: string
+  let otherAcc: SignerWithAddress
+  let other: string
+  let join: Join
+  let joinFromOther: Join
+  let token: ERC20Mock
 
-  it("test all", async () => {
-    const assets = jsonToMap(
-      fs.readFileSync("./output/assets.json", "utf8")
-    ) as Map<string, string>;
-    const joins = jsonToMap(
-      fs.readFileSync("./output/joins.json", "utf8")
-    ) as Map<string, string>;
+  it('test all', async () => {
+    const assets = jsonToMap(fs.readFileSync('./output/assets.json', 'utf8')) as Map<string, string>
+    const joins = jsonToMap(fs.readFileSync('./output/joins.json', 'utf8')) as Map<string, string>
 
-    const signers = await ethers.getSigners();
-    ownerAcc = signers[0];
-    owner = await ownerAcc.getAddress();
+    const signers = await ethers.getSigners()
+    ownerAcc = signers[0]
+    owner = await ownerAcc.getAddress()
 
-    otherAcc = signers[1];
-    other = await otherAcc.getAddress();
+    otherAcc = signers[1]
+    other = await otherAcc.getAddress()
 
-    token = (await ethers.getContractAt(
-      "ERC20Mock",
-      assets.get(WBTC) as string,
-      ownerAcc
-    )) as ERC20Mock;
-    join = (await ethers.getContractAt(
-      "Join",
-      joins.get(WBTC) as string,
-      ownerAcc
-    )) as Join;
-    joinFromOther = join.connect(otherAcc);
+    token = (await ethers.getContractAt('ERC20Mock', assets.get(WBTC) as string, ownerAcc)) as ERC20Mock
+    join = (await ethers.getContractAt('Join', joins.get(WBTC) as string, ownerAcc)) as Join
+    joinFromOther = join.connect(otherAcc)
 
-    await join.grantRoles(
-      [id("join(address,uint128)"), id("exit(address,uint128)")],
-      owner
-    );
+    await join.grantRoles([id('join(address,uint128)'), id('exit(address,uint128)')], owner)
 
-    await token.mint(owner, WAD.mul(4));
+    await token.mint(owner, WAD.mul(4))
 
-    console.log("pulls tokens from user");
-    let storedBalanceBefore = await join.storedBalance();
-    await token.approve(join.address, WAD);
+    console.log('pulls tokens from user')
+    let storedBalanceBefore = await join.storedBalance()
+    await token.approve(join.address, WAD)
     expect(await join.join(owner, WAD))
-      .to.emit(token, "Transfer")
-      .withArgs(owner, join.address, WAD);
-    expect(await join.storedBalance()).to.equal(storedBalanceBefore.add(WAD));
+      .to.emit(token, 'Transfer')
+      .withArgs(owner, join.address, WAD)
+    expect(await join.storedBalance()).to.equal(storedBalanceBefore.add(WAD))
 
-    console.log("accepts surplus as a transfer");
-    storedBalanceBefore = await join.storedBalance();
-    await token.transfer(join.address, WAD);
-    expect(await join.join(owner, WAD)).to.not.emit(token, "Transfer");
-    expect(await join.storedBalance()).to.equal(storedBalanceBefore.add(WAD));
+    console.log('accepts surplus as a transfer')
+    storedBalanceBefore = await join.storedBalance()
+    await token.transfer(join.address, WAD)
+    expect(await join.join(owner, WAD)).to.not.emit(token, 'Transfer')
+    expect(await join.storedBalance()).to.equal(storedBalanceBefore.add(WAD))
 
-    console.log("combines surplus and tokens pulled from the user");
-    storedBalanceBefore = await join.storedBalance();
-    await token.transfer(join.address, WAD);
-    await token.approve(join.address, WAD);
+    console.log('combines surplus and tokens pulled from the user')
+    storedBalanceBefore = await join.storedBalance()
+    await token.transfer(join.address, WAD)
+    await token.approve(join.address, WAD)
     expect(await join.join(owner, WAD.mul(2)))
-      .to.emit(token, "Transfer")
-      .withArgs(owner, join.address, WAD);
-    expect(await join.storedBalance()).to.equal(
-      storedBalanceBefore.add(WAD.mul(2))
-    );
+      .to.emit(token, 'Transfer')
+      .withArgs(owner, join.address, WAD)
+    expect(await join.storedBalance()).to.equal(storedBalanceBefore.add(WAD.mul(2)))
 
-    console.log("pushes tokens to user");
-    storedBalanceBefore = await join.storedBalance();
+    console.log('pushes tokens to user')
+    storedBalanceBefore = await join.storedBalance()
     expect(await join.exit(owner, WAD))
-      .to.emit(token, "Transfer")
-      .withArgs(join.address, owner, WAD);
-    expect(await join.storedBalance()).to.equal(storedBalanceBefore.sub(WAD));
-  });
-});
+      .to.emit(token, 'Transfer')
+      .withArgs(join.address, owner, WAD)
+    expect(await join.storedBalance()).to.equal(storedBalanceBefore.sub(WAD))
+  })
+})
--- test/091_governance_bundles.ts
+++ test/091_governance_bundles.ts
@@ -1,164 +1,128 @@
-import {
-  CHI,
-  RATE,
-  WAD,
-  MAX128 as MAX,
-  DAI,
-  USDC,
-  VAULT_OPS as OPS,
-} from "../shared/constants";
-import { id } from "@yield-protocol/utils-v2";
+import { CHI, RATE, WAD, MAX128 as MAX, DAI, USDC, VAULT_OPS as OPS } from '../shared/constants'
+import { id } from '@yield-protocol/utils-v2'
 
-import { Cauldron } from "../typechain/Cauldron";
-import { FYToken } from "../typechain/FYToken";
-import { Join } from "../typechain/Join";
-import { Pool } from "../typechain/Pool";
-import { PoolFactory } from "../typechain/PoolFactory";
-import { IOracle } from "../typechain/IOracle";
-import { ERC20Mock } from "../typechain/ERC20Mock";
-import { ISourceMock } from "../typechain/ISourceMock";
+import { Cauldron } from '../typechain/Cauldron'
+import { FYToken } from '../typechain/FYToken'
+import { Join } from '../typechain/Join'
+import { Pool } from '../typechain/Pool'
+import { PoolFactory } from '../typechain/PoolFactory'
+import { IOracle } from '../typechain/IOracle'
+import { ERC20Mock } from '../typechain/ERC20Mock'
+import { ISourceMock } from '../typechain/ISourceMock'
 
-import { LadleWrapper } from "../shared/ladleWrapper";
+import { LadleWrapper } from '../shared/ladleWrapper'
 
-import ERC20MockArtifact from "../artifacts/contracts/mocks/ERC20Mock.sol/ERC20Mock.json";
-import ChainlinkAggregatorV3MockArtifact from "../artifacts/contracts/mocks/ChainlinkAggregatorV3Mock.sol/ChainlinkAggregatorV3Mock.json";
-import CTokenRateMockArtifact from "../artifacts/contracts/mocks/CTokenRateMock.sol/CTokenRateMock.json";
-import CTokenChiMockArtifact from "../artifacts/contracts/mocks/CTokenChiMock.sol/CTokenChiMock.json";
+import ERC20MockArtifact from '../artifacts/contracts/mocks/ERC20Mock.sol/ERC20Mock.json'
+import ChainlinkAggregatorV3MockArtifact from '../artifacts/contracts/mocks/ChainlinkAggregatorV3Mock.sol/ChainlinkAggregatorV3Mock.json'
+import CTokenRateMockArtifact from '../artifacts/contracts/mocks/CTokenRateMock.sol/CTokenRateMock.json'
+import CTokenChiMockArtifact from '../artifacts/contracts/mocks/CTokenChiMock.sol/CTokenChiMock.json'
 
-import JoinArtifact from "../artifacts/@yield-protocol/vault-v2/contracts/Join.sol/Join.json";
-import FYTokenArtifact from "../artifacts/@yield-protocol/vault-v2/contracts/FYToken.sol/FYToken.json";
+import JoinArtifact from '../artifacts/@yield-protocol/vault-v2/contracts/Join.sol/Join.json'
+import FYTokenArtifact from '../artifacts/@yield-protocol/vault-v2/contracts/FYToken.sol/FYToken.json'
 
-import { ethers, waffle } from "hardhat";
-import { expect } from "chai";
+import { ethers, waffle } from 'hardhat'
+import { expect } from 'chai'
 
-import { VaultEnvironment } from "../fixtures/vault";
-import { fixture } from "../environments/testing";
-import { generateMaturities } from "../shared/helpers";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { VaultEnvironment } from '../fixtures/vault'
+import { fixture } from '../environments/testing'
+import { generateMaturities } from '../shared/helpers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
-const { deployContract, loadFixture } = waffle;
+const { deployContract, loadFixture } = waffle
 
-describe("Governance", function () {
-  this.timeout(0);
+describe('Governance', function () {
+  this.timeout(0)
 
-  let env: VaultEnvironment;
+  let env: VaultEnvironment
 
-  let owner: SignerWithAddress;
+  let owner: SignerWithAddress
 
-  let cauldron: Cauldron;
-  let ladle: LadleWrapper;
-  let chiRateOracle: IOracle;
-  let spotOracle: IOracle;
+  let cauldron: Cauldron
+  let ladle: LadleWrapper
+  let chiRateOracle: IOracle
+  let spotOracle: IOracle
 
-  let base: ERC20Mock;
-  let ilk: ERC20Mock;
-  let spotSource: ISourceMock;
-  let chiSource: ISourceMock;
-  let rateSource: ISourceMock;
+  let base: ERC20Mock
+  let ilk: ERC20Mock
+  let spotSource: ISourceMock
+  let chiSource: ISourceMock
+  let rateSource: ISourceMock
 
-  let baseJoin: Join;
-  let ilkJoin: Join;
-  let maturity: Number;
-  let fyToken: FYToken;
-  let pool: Pool;
-  let factory: PoolFactory;
+  let baseJoin: Join
+  let ilkJoin: Join
+  let maturity: Number
+  let fyToken: FYToken
+  let pool: Pool
+  let factory: PoolFactory
 
-  let seriesId = ethers.utils.hexlify(ethers.utils.randomBytes(6));
-  let baseId = ethers.utils.hexlify(ethers.utils.randomBytes(6));
-  let ilkId = ethers.utils.hexlify(ethers.utils.randomBytes(6));
+  let seriesId = ethers.utils.hexlify(ethers.utils.randomBytes(6))
+  let baseId = ethers.utils.hexlify(ethers.utils.randomBytes(6))
+  let ilkId = ethers.utils.hexlify(ethers.utils.randomBytes(6))
 
-  it("governance", async () => {
-    env = await loadFixture(fixture);
+  it('governance', async () => {
+    env = await loadFixture(fixture)
 
-    owner = (await ethers.getSigners())[0];
+    owner = (await ethers.getSigners())[0]
 
-    cauldron = env.cauldron as Cauldron;
-    ladle = env.ladle as LadleWrapper;
-    factory = env.poolFactory as PoolFactory;
-    chiRateOracle = env.oracles.get(CHI) as unknown as IOracle;
-    spotOracle = env.oracles.get(USDC) as unknown as IOracle;
+    cauldron = env.cauldron as Cauldron
+    ladle = env.ladle as LadleWrapper
+    factory = env.poolFactory as PoolFactory
+    chiRateOracle = env.oracles.get(CHI) as unknown as IOracle
+    spotOracle = env.oracles.get(USDC) as unknown as IOracle
 
-    base = (await deployContract(owner, ERC20MockArtifact, [
-      baseId,
-      baseId,
-    ])) as ERC20Mock;
-    ilk = (await deployContract(owner, ERC20MockArtifact, [
-      ilkId,
-      ilkId,
-    ])) as ERC20Mock;
-    spotSource = (await deployContract(
-      owner,
-      ChainlinkAggregatorV3MockArtifact,
-      []
-    )) as ISourceMock;
-    rateSource = (await deployContract(
-      owner,
-      CTokenRateMockArtifact,
-      []
-    )) as ISourceMock;
-    await rateSource.set(WAD.mul(2));
-    chiSource = (await deployContract(
-      owner,
-      CTokenChiMockArtifact,
-      []
-    )) as ISourceMock;
-    await chiSource.set(WAD.mul(3));
+    base = (await deployContract(owner, ERC20MockArtifact, [baseId, baseId])) as ERC20Mock
+    ilk = (await deployContract(owner, ERC20MockArtifact, [ilkId, ilkId])) as ERC20Mock
+    spotSource = (await deployContract(owner, ChainlinkAggregatorV3MockArtifact, [])) as ISourceMock
+    rateSource = (await deployContract(owner, CTokenRateMockArtifact, [])) as ISourceMock
+    await rateSource.set(WAD.mul(2))
+    chiSource = (await deployContract(owner, CTokenChiMockArtifact, [])) as ISourceMock
+    await chiSource.set(WAD.mul(3))
 
-    maturity = (await generateMaturities(1))[0];
+    maturity = (await generateMaturities(1))[0]
     // })
 
     // it('add an asset', async () => {
     // 4b, 6c
 
     // Add asset in Cauldron
-    await cauldron.addAsset(baseId, base.address);
+    await cauldron.addAsset(baseId, base.address)
 
     // Deploy Join and integrate it with Ladle
-    baseJoin = (await deployContract(owner, JoinArtifact, [
-      base.address,
-    ])) as Join;
-    await ladle.addJoin(baseId, baseJoin.address);
-    await baseJoin.grantRoles(
-      [id("join(address,uint128)"), id("exit(address,uint128)")],
-      ladle.address
-    );
+    baseJoin = (await deployContract(owner, JoinArtifact, [base.address])) as Join
+    await ladle.addJoin(baseId, baseJoin.address)
+    await baseJoin.grantRoles([id('join(address,uint128)'), id('exit(address,uint128)')], ladle.address)
     // })
 
     // it('make a base out of an asset', async () => {
     // Asset, 4d
 
     // Add source to multioracle
-    await chiRateOracle.setSources([baseId], [RATE], [rateSource.address]);
+    await chiRateOracle.setSources([baseId], [RATE], [rateSource.address])
 
     // Add oracle for base in Cauldron
-    await cauldron.setRateOracle(baseId, chiRateOracle.address);
+    await cauldron.setRateOracle(baseId, chiRateOracle.address)
     // })
 
     // it('make a collateral out of an asset', async () => {
     // Base, Asset, 4e, 4c
 
     // Asset - Add asset in Cauldron
-    await cauldron.addAsset(ilkId, ilk.address);
+    await cauldron.addAsset(ilkId, ilk.address)
 
     // Deploy Join and integrate it with Ladle
-    ilkJoin = (await deployContract(owner, JoinArtifact, [
-      ilk.address,
-    ])) as Join;
-    await ladle.addJoin(ilkId, ilkJoin.address);
-    await ilkJoin.grantRoles(
-      [id("join(address,uint128)"), id("exit(address,uint128)")],
-      ladle.address
-    );
+    ilkJoin = (await deployContract(owner, JoinArtifact, [ilk.address])) as Join
+    await ladle.addJoin(ilkId, ilkJoin.address)
+    await ilkJoin.grantRoles([id('join(address,uint128)'), id('exit(address,uint128)')], ladle.address)
 
     // 4e - Add source to multioracle
-    await spotOracle.setSources([baseId], [ilkId], [spotSource.address]);
+    await spotOracle.setSources([baseId], [ilkId], [spotSource.address])
 
     // Add oracle for base/ilk pair in Cauldron
-    const ratio = 1000000; //  1000000 == 100% collateralization ratio
-    await cauldron.setSpotOracle(baseId, ilkId, spotOracle.address, ratio);
+    const ratio = 1000000 //  1000000 == 100% collateralization ratio
+    await cauldron.setSpotOracle(baseId, ilkId, spotOracle.address, ratio)
 
     // 4c - Set max debt for the pair
-    await cauldron.setMaxDebt(baseId, ilkId, WAD.mul(1000000));
+    await cauldron.setMaxDebt(baseId, ilkId, WAD.mul(1000000))
     // })
 
     // it('add a series', async () => {
@@ -172,43 +136,30 @@ describe("Governance", function () {
       maturity,
       seriesId,
       seriesId,
-    ])) as FYToken;
+    ])) as FYToken
 
     // 4f - Add fyToken/series to the Cauldron
-    await cauldron.addSeries(seriesId, baseId, fyToken.address);
+    await cauldron.addSeries(seriesId, baseId, fyToken.address)
 
     // Grant access to Join for fyToken to redeem
-    await baseJoin.grantRoles(
-      [id("join(address,uint128)"), id("exit(address,uint128)")],
-      fyToken.address
-    );
+    await baseJoin.grantRoles([id('join(address,uint128)'), id('exit(address,uint128)')], fyToken.address)
 
     // Grant access to fyToken for Ladle to issue and cancel
-    await fyToken.grantRoles(
-      [id("mint(address,uint256)"), id("burn(address,uint256)")],
-      ladle.address
-    );
+    await fyToken.grantRoles([id('mint(address,uint256)'), id('burn(address,uint256)')], ladle.address)
 
     // 4g - Add relevant ilks to series
-    await cauldron.addIlks(seriesId, [ilkId]);
+    await cauldron.addIlks(seriesId, [ilkId])
 
     // 5a - Deploy Pool
-    const calculatedAddress = await factory.calculatePoolAddress(
-      base.address,
-      fyToken.address
-    );
-    await factory.createPool(base.address, fyToken.address);
-    const pool = (await ethers.getContractAt(
-      "Pool",
-      calculatedAddress,
-      owner
-    )) as unknown as Pool;
+    const calculatedAddress = await factory.calculatePoolAddress(base.address, fyToken.address)
+    await factory.createPool(base.address, fyToken.address)
+    const pool = (await ethers.getContractAt('Pool', calculatedAddress, owner)) as unknown as Pool
 
     // Initialize pool
-    await base.mint(pool.address, WAD.mul(1000000));
-    await pool.mint(await owner.getAddress(), true, 0);
+    await base.mint(pool.address, WAD.mul(1000000))
+    await pool.mint(await owner.getAddress(), true, 0)
 
     // 6d - Add pool to Ladle
-    await ladle.addPool(seriesId, pool.address);
-  });
-});
+    await ladle.addPool(seriesId, pool.address)
+  })
+})
