d2e998d76ed0314b6a64ab4f87d6010332db7320
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-08-26 04:23:49 +0100

    feat: rateSource script

---
+++ scripts/rateSource.ts
@@ -0,0 +1,46 @@
+/**
+ * @dev This script replaces one or more rate data sources in the CompoundMultiOracle.
+ * 
+ * It takes as inputs the governance, protocol and rateSources json address files.
+ * It rewrites the rateSources json address file.
+ */
+
+import { ethers } from 'hardhat'
+import *  as fs from 'fs'
+import { bytesToString, mapToJson, jsonToMap } from '../shared/helpers'
+import { RATE, ETH, DAI, USDC, WBTC, USDT } from '../shared/constants'
+
+import { CompoundMultiOracle } from '../typechain/CompoundMultiOracle'
+import { Timelock } from '../typechain/Timelock'
+
+(async () => {
+  const newSources: Array<[string, string]> = [
+    [DAI, "0x78F3579FbBb4a9894cE27cC201216Ef46A132f1c"],
+    [USDC, "0x625E23f0C081CF8a2bbb87738087D8b0A5f5F412"],
+    [USDT, "0xBce93F0D091092D780C6A386fb6d34780FFb031C"],
+  ]
+  const [ ownerAcc ] = await ethers.getSigners();
+  const governance = jsonToMap(fs.readFileSync('./output/governance.json', 'utf8')) as Map<string, string>;
+  const protocol = jsonToMap(fs.readFileSync('./output/protocol.json', 'utf8')) as Map<string,string>;
+  const rateSources = jsonToMap(fs.readFileSync('./output/rateSources.json', 'utf8')) as Map<string, string>;
+
+  const compoundOracle = await ethers.getContractAt('CompoundMultiOracle', protocol.get('compoundOracle') as string, ownerAcc) as unknown as CompoundMultiOracle
+  const timelock = await ethers.getContractAt('Timelock', governance.get('timelock') as string, ownerAcc) as unknown as Timelock
+
+  const proposal : Array<{ target: string; data: string}> = []
+  for (let [baseId, sourceAddress] of newSources) {
+    proposal.push({
+      target: compoundOracle.address,
+      data: compoundOracle.interface.encodeFunctionData("setSource", [baseId, RATE, sourceAddress])
+    })
+    console.log(`[Rate: ${bytesToString(baseId)}: ${rateSources.get(baseId) || undefined} -> ${sourceAddress}],`)
+    rateSources.set(baseId, sourceAddress)
+  }
+  const txHash = await timelock.callStatic.propose(proposal)
+  await timelock.propose(proposal); console.log(`Proposed ${txHash}`)
+
+  fs.writeFileSync('./output/rateSources.json', mapToJson(rateSources), 'utf8')
+
+  await timelock.approve(txHash); console.log(`Approved ${txHash}`)
+  await timelock.execute(proposal); console.log(`Executed ${txHash}`)
+})()
