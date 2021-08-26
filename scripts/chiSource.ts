6e933c74f4972528d891c9beb90eed3bd97f25ef
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-08-26 04:26:31 +0100

    feat: chiSource script

---
+++ scripts/chiSource.ts
@@ -0,0 +1,46 @@
+/**
+ * @dev This script replaces one or more chi data sources in the CompoundMultiOracle.
+ * 
+ * It takes as inputs the governance, protocol and chiSources json address files.
+ * It rewrites the chiSources json address file.
+ */
+
+import { ethers } from 'hardhat'
+import *  as fs from 'fs'
+import { bytesToString, mapToJson, jsonToMap } from '../shared/helpers'
+import { CHI, ETH, DAI, USDC, WBTC, USDT } from '../shared/constants'
+
+import { CompoundMultiOracle } from '../typechain/CompoundMultiOracle'
+import { Timelock } from '../typechain/Timelock'
+
+(async () => {
+  const newSources: Array<[string, string]> = [
+    [DAI,  "0xD9D6D61bc216a0BE1EecA4155b258CbB3030d23f"],
+    [USDC, "0x078FfF3582342a16a7b038E6F4Fc9E88F738143d"],
+    [USDT, "0x1292BAe0Ba398F9e480251b8C40D2A171EC446D1"],
+  ]
+  const [ ownerAcc ] = await ethers.getSigners();
+  const governance = jsonToMap(fs.readFileSync('./output/governance.json', 'utf8')) as Map<string, string>;
+  const protocol = jsonToMap(fs.readFileSync('./output/protocol.json', 'utf8')) as Map<string,string>;
+  const chiSources = jsonToMap(fs.readFileSync('./output/chiSources.json', 'utf8')) as Map<string, string>;
+
+  const compoundOracle = await ethers.getContractAt('CompoundMultiOracle', protocol.get('compoundOracle') as string, ownerAcc) as unknown as CompoundMultiOracle
+  const timelock = await ethers.getContractAt('Timelock', governance.get('timelock') as string, ownerAcc) as unknown as Timelock
+
+  const proposal : Array<{ target: string; data: string}> = []
+  for (let [baseId, sourceAddress] of newSources) {
+    proposal.push({
+      target: compoundOracle.address,
+      data: compoundOracle.interface.encodeFunctionData("setSource", [baseId, CHI, sourceAddress])
+    })
+    console.log(`[Chi: ${bytesToString(baseId)}: ${chiSources.get(baseId) || undefined} -> ${sourceAddress}],`)
+    chiSources.set(baseId, sourceAddress)
+  }
+  const txHash = await timelock.callStatic.propose(proposal)
+  await timelock.propose(proposal); console.log(`Proposed ${txHash}`)
+
+  fs.writeFileSync('./output/chiSources.json', mapToJson(chiSources), 'utf8')
+
+  await timelock.approve(txHash); console.log(`Approved ${txHash}`)
+  await timelock.execute(proposal); console.log(`Executed ${txHash}`)
+})()
