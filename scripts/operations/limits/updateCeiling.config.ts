c63bc001e77bec220b8731123c0a02750ed00fc8
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-10-28 14:02:50 +0100

    tweak: raise ceiling limits for DAI/ETH and USDC/ETH

--- scripts/operations/limits/updateCeiling.config.ts
+++ scripts/operations/limits/updateCeiling.config.ts
@@ -6,6 +6,6 @@ import { ETH, DAI, USDC, WBTC, USDT } from '../../../shared/constants'
 
 // Input data: baseId, ilkId, maxDebt
 export const newMax: Array<[string, string, number]> = [
-  [DAI, ETH, 250000],
-  [USDC, ETH, 250000],
+  [DAI, ETH, 500000],
+  [USDC, ETH, 500000],
 ]
