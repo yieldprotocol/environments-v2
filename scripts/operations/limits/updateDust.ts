c36529f08addfc9022b40cee9ff61f20e2f02717
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-10-28 13:56:27 +0100

    tweak: better logs

--- scripts/operations/limits/updateDust.ts
+++ scripts/operations/limits/updateDust.ts
@@ -27,15 +27,16 @@ import { newMin } from './updateDust.config'
   const protocol = jsonToMap(fs.readFileSync('./addresses/protocol.json', 'utf8')) as Map<string, string>
 
   let [ownerAcc] = await ethers.getSigners()
-  // If we are running in a mainnet fork, the account used is the default hardhat one, we can detect that and impersonate the deployer
-  if (ownerAcc.address === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266") {
-    const deployer = governance.get('deployer') as string
-    console.log(`Running on a fork, impersonating ${deployer}`)
+  // If we are running in a mainnet fork, the account used is the default hardhat one, we can detect that and impersonate a developer
+  const on_fork = (ownerAcc.address === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+  if (on_fork) {
+    const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
+    console.log(`Running on a fork, impersonating ${developer}`)
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: [deployer],
+      params: [developer],
     });
-    ownerAcc = await ethers.getSigner(deployer)
+    ownerAcc = await ethers.getSigner(developer)
   }
   
   // Contract instantiation
@@ -66,34 +67,27 @@ import { newMin } from './updateDust.config'
   const txHash = await timelock.hash(proposal)
   console.log(`Proposal: ${txHash}`)
 
-  // If we are running in a mainnet fork, the account used is the default hardhat one, we can detect that and impersonate the deployer
-  if (ownerAcc.address === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266") {
-    const deployer = governance.get('deployer') as string
-    console.log(`Running on a fork, impersonating ${deployer}`)
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [deployer],
-    });
-    ownerAcc = await ethers.getSigner(deployer)
-  }
-
   // Depending on the proposal state, propose, approve (if in a fork, impersonating the multisig), or execute
   if ((await timelock.proposals(txHash)).state === 0) { // Propose
     await timelock.propose(proposal)
     while ((await timelock.proposals(txHash)).state < 1) {}
     console.log(`Proposed ${txHash}`)
   } else if ((await timelock.proposals(txHash)).state === 1) { // Approve, impersonating multisig if in a fork
-    // If running on a mainnet fork, impersonating the multisig will work
-    const multisig = governance.get('multisig') as string
-    console.log(`Running on a fork, impersonating multisig at ${multisig}`)
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [multisig],
-    });
-    const multisigAcc = await ethers.getSigner(multisig)
-    await timelock.connect(multisigAcc).approve(txHash)
-    while ((await timelock.proposals(txHash)).state < 2) {}
-    console.log(`Approved ${txHash}`)
+    if (on_fork){
+      // If running on a mainnet fork, impersonating the multisig will work
+      const multisig = governance.get('multisig') as string
+      console.log(`Running on a fork, impersonating multisig at ${multisig}`)
+      await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [multisig],
+      });
+      const multisigAcc = await ethers.getSigner(multisig)
+      await timelock.connect(multisigAcc).approve(txHash)
+      while ((await timelock.proposals(txHash)).state < 2) {}
+      console.log(`Approved ${txHash}`)
+    } else {
+      console.log("Can't do, go bug the multisig owners");
+    }
   } else if ((await timelock.proposals(txHash)).state === 2) { // Execute
     await timelock.execute(proposal)
     while ((await timelock.proposals(txHash)).state > 0) {}
