3daaf861b1fe8fb155f22db923c6a6a5874673ec
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-11-04 15:40:59 +0000

    feat: faster execution

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,62 +3,12 @@ import * as path from 'path'
 
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
+import '@typechain/hardhat'
 import 'hardhat-abi-exporter'
 import 'hardhat-contract-sizer'
 import 'hardhat-gas-reporter'
-import 'hardhat-typechain'
 import 'solidity-coverage'
 import 'hardhat-deploy'
-import { task } from 'hardhat/config'
-
-// import { addAsset, makeBase, makeIlk, addSeries } from './scripts/add'
-
-// REQUIRED TO ENSURE METADATA IS SAVED IN DEPLOYMENTS (because solidity-coverage disable it otherwise)
-/* import {
-  TASK_COMPILE_GET_COMPILER_INPUT
-} from "hardhat/builtin-tasks/task-names"
-task(TASK_COMPILE_GET_COMPILER_INPUT).setAction(async (_, bre, runSuper) => {
-  const input = await runSuper()
-  input.settings.metadata.useLiteralContent = bre.network.name !== "coverage"
-  return input
-}) */
-
-/* task("asset", "Adds assets and makes them into ilks and/or bases")
-  .addFlag("add", "Add asset")
-  .addFlag("base", "Make asset into base")
-  .addFlag("ilk", "Make asset into ilk")
-  .addVariadicPositionalParam("asset", "The details of the asset")
-  .setAction(async (taskArgs, hre) => {
-    const argv: any = {}
-    if (taskArgs.add) {
-      argv.asset = taskArgs.asset[0]  // address
-      await addAsset(argv, hre)
-    } else if (taskArgs.base) {
-      argv.asset = taskArgs.asset[0]  // address
-      argv.rateSource = [1]           // address
-      argv.chiSource = [2]            // address
-      await makeBase(argv, hre)
-    } else if (taskArgs.ilk) {
-      argv.asset = taskArgs.asset[0]  // address, p.e. MKR, which will be used as collateral
-      argv.base = taskArgs.asset[1]   // address, p.e. DAI, which will be the underlying
-      argv.spotSource = taskArgs.asset[2] // address, p.e. DAI/MKR, which will be the source for the spot oracle
-      await makeIlk(argv, hre)
-    } else {
-      console.error("Must add an asset, make an asset into a base or make an asset into an ilk")
-    }
-});
-
-task("series", "Adds a series")
-  .addVariadicPositionalParam("series", "The details of the series")
-  .setAction(async (taskArgs, hre) => {
-    const argv: any = {}
-    argv.seriesId = taskArgs.series[0]  // address, p.e. MKR, which will be used as collateral
-    argv.base = taskArgs.series[1]   // address, p.e. DAI, which will be the underlying
-    argv.maturity = taskArgs.series[2]   // address, p.e. DAI, which will be the underlying
-    argv.ilkIds = []
-    argv.ilkIds = taskArgs.series.slice(3).forEach((ilkId: any) => { argv.ilkIds.push(ilkId) })
-    await addSeries(argv, hre)
-}); */
 
 function nodeUrl(network: any) {
   let infuraKey
