045b841a26f17e4c6639b9ccbe5f70e647dff7f8
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-11-28 06:42:16 +0000

    feat: generalized adding simple collaterals

---
+++ scripts/governance/addChainlinkCollateral/addChainlinkCollateral-2.ts
@@ -0,0 +1,76 @@
+import { ethers } from 'hardhat'
+import { getOriginalChainId, readAddressMappingIfExists, proposeApproveExecute, getOwnerOrImpersonate } from '../../../shared/helpers'
+
+import { updateChainlinkSourcesProposal } from '../../fragments/oracles/updateChainlinkSourcesProposal'
+import { orchestrateAddedAssetProposal } from '../../fragments/assetsAndSeries/orchestrateAddedAssetProposal'
+import { makeIlkProposal } from '../../fragments/assetsAndSeries/makeIlkProposal'
+import { addIlksToSeriesProposal } from '../../fragments/assetsAndSeries/addIlksToSeriesProposal'
+
+import { IOracle, ChainlinkMultiOracle, Cauldron, Ladle, Witch, Wand, Timelock, EmergencyBrake } from '../../../typechain'
+
+import { developer, chainlinkSources, assetToAdd, limits, seriesIlks } from './addUNI.config'
+
+/**
+ * @dev This script configures the Yield Protocol to use a collateral with a Chainlink oracle vs. ETH.
+ * Previously, the collateral should have been added as an asset with the Wand.
+ * Add collateral as an asset
+ * --- You are here ---
+ * Add the collateral/ETH source to the Chainlink Oracle
+ * Permission the collateral Join
+ * Make collateral into an Ilk
+ * Approve collateral as collateral for all series
+ */
+
+;(async () => {
+  const chainId = await getOriginalChainId()
+  if (chainId !== 1 && chainId !== 42) throw "Only Kovan and Mainnet supported"
+
+  let ownerAcc = await getOwnerOrImpersonate(developer.get(chainId) as string)
+
+  const protocol = readAddressMappingIfExists('protocol.json');
+  const governance = readAddressMappingIfExists('governance.json');
+
+  const chainlinkOracle = (await ethers.getContractAt(
+    'ChainlinkMultiOracle',
+    protocol.get('chainlinkOracle') as string,
+    ownerAcc
+  )) as unknown as ChainlinkMultiOracle
+  const cauldron = (await ethers.getContractAt(
+    'Cauldron',
+    protocol.get('cauldron') as string,
+    ownerAcc
+  )) as unknown as Cauldron
+  const ladle = (await ethers.getContractAt(
+    'Ladle',
+    protocol.get('ladle') as string,
+    ownerAcc
+  )) as unknown as Ladle
+  const witch = (await ethers.getContractAt(
+    'Witch',
+    protocol.get('witch') as string,
+    ownerAcc
+  )) as unknown as Witch
+  const wand = (await ethers.getContractAt(
+    'Wand',
+    protocol.get('wand') as string,
+    ownerAcc
+  )) as unknown as Wand
+  const cloak = (await ethers.getContractAt(
+    'EmergencyBrake',
+    governance.get('cloak') as string,
+    ownerAcc
+  )) as unknown as EmergencyBrake
+  const timelock = (await ethers.getContractAt(
+    'Timelock',
+    governance.get('timelock') as string,
+    ownerAcc
+  )) as unknown as Timelock
+
+  let proposal: Array<{ target: string; data: string }> = []
+  proposal = proposal.concat(await updateChainlinkSourcesProposal(chainlinkOracle, chainlinkSources.get(chainId) as [string, string, string, string, string][]))
+  proposal = proposal.concat(await orchestrateAddedAssetProposal(ownerAcc, ladle, timelock, cloak, [assetToAdd.get(chainId) as [string, string]]))
+  proposal = proposal.concat(await makeIlkProposal(ownerAcc, chainlinkOracle as unknown as IOracle, ladle, witch, wand, cloak, limits as [string, string, string, number, number, number, number, number][]))
+  proposal = proposal.concat(await addIlksToSeriesProposal(cauldron, seriesIlks))
+
+  await proposeApproveExecute(timelock, proposal, governance.get('multisig') as string)
+})()
---
+++ scripts/governance/addChainlinkCollateral/addUNI.config.ts
@@ -0,0 +1,62 @@
+ /**
+ * @dev Input file for adding UNI as a collateral
+ */
+
+import { ETH, DAI, USDC, UNI } from '../../../shared/constants'
+import { FYDAI2112, FYDAI2203, FYUSDC2112, FYUSDC2203 } from '../../../shared/constants'
+import { CHAINLINK } from '../../../shared/constants'
+
+export const developer: Map<number, string> = new Map([
+  [1, '0xC7aE076086623ecEA2450e364C838916a043F9a8'],
+  [42, '0x5AD7799f02D5a829B2d6FA085e6bd69A872619D5'],
+])
+
+export const assets: Map<number, Map<string, string>> = new Map([
+  [1, new Map([
+    [ETH,    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+    [DAI,    '0x6B175474E89094C44Da98b954EedeAC495271d0F'],
+    [USDC,   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
+    [UNI,    '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'],
+  ])],
+  [42, new Map([
+    [ETH,    '0x55C0458edF1D8E07DF9FB44B8960AecC515B4492'],
+    [DAI,    '0xaFCdc724EB8781Ee721863db1B15939675996484'],
+    [USDC,   '0xeaCB3AAB4CA68F1e6f38D56bC5FCc499B76B4e2D'],
+    [UNI,    '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'],
+  ])],
+])
+  
+
+export const chainlinkSources: Map<number, Array<[string, string, string, string, string]>> = new Map([
+  [1, [
+    [UNI,   (assets.get(1) as Map<string, string>).get(DAI)   as string, ETH, (assets.get(1) as Map<string, string>).get(ETH) as string, '0xD6aA3D25116d8dA79Ea0246c4826EB951872e02e'],
+  ]],
+  [42, [
+    [UNI,   (assets.get(42) as Map<string, string>).get(DAI)   as string, ETH, (assets.get(42) as Map<string, string>).get(ETH) as string, '0xC0E69E49D98D26D52f7505Af1dF8b3009168f945'],
+  ]],
+])
+
+export const whale = new Map([
+  [1, '0x47173b170c64d16393a52e6c480b3ad8c302ba1e'],
+  [42, '0x41653c7d61609d856f29355e404f310ec4142cfb'],
+])
+
+// Assets for which we will have a Join
+export const assetToAdd: Map<number, [string, string]> = new Map([
+  [1,  [UNI, (assets.get(1) as Map<string, string>).get(UNI) as string]],
+  [42, [UNI, (assets.get(42) as Map<string, string>).get(UNI) as string]],
+])
+
+// Input data: baseId, ilkId, oracle name, ratio (1000000 == 100%), inv(ratio), line, dust, dec
+export const limits: Array<[string, string, string, number, number, number, number, number]> = [
+  [DAI, UNI, CHAINLINK, 2000000, 500000, 250000, 100, 18],
+  [USDC, UNI, CHAINLINK, 2000000, 500000, 250000, 100, 6],
+]
+
+// Input data: seriesId, [ilkId]
+export const seriesIlks: Array<[string, string[]]> = [
+  [FYDAI2112,  [UNI]],
+  [FYDAI2203,  [UNI]],
+  [FYUSDC2112, [UNI]],
+  [FYUSDC2203, [UNI]],
+]
