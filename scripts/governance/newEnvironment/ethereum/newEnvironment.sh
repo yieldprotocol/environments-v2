548a8a58c9a7fa10a5faef167fbde80083100fff
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-11-30 07:15:01 +0000

    fix: better script

--- scripts/governance/newEnvironment/ethereum/newEnvironment.sh
+++ scripts/governance/newEnvironment/ethereum/newEnvironment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -euxo pipefail
+set -eux
 RUN="npx hardhat run --network localhost --no-compile"
 HERE=$(dirname $0)
 
@@ -13,9 +13,9 @@ $RUN scripts/fragments/core/libraries/deploySafeERC20Namer.ts
 # Phase 2: Governance
 $RUN scripts/fragments/core/governance/deployTimelock.ts
 $RUN scripts/fragments/core/governance/deployCloak.ts
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-07.ts # orchestrate Cloak - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-07.ts # orchestrate Cloak - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-07.ts # orchestrate Cloak - execute
+$RUN $HERE/newEnvironment-07.ts # orchestrate Cloak - propose
+$RUN $HERE/newEnvironment-07.ts # orchestrate Cloak - approve
+$RUN $HERE/newEnvironment-07.ts # orchestrate Cloak - execute
 
 # Phase 3: Oracles
 $RUN scripts/fragments/oracles/deployChainlinkOracle.ts
@@ -24,44 +24,44 @@ $RUN scripts/fragments/oracles/deployCompositeOracle.ts
 $RUN scripts/fragments/oracles/deployLidoOracle.ts
 $RUN scripts/fragments/oracles/deployUniswapOracle.ts
 
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-13.ts # setup oracles, data sources and price derivation paths - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-13.ts # setup oracles, data sources and price derivation paths - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-13.ts # setup oracles, data sources and price derivation paths - execute
+$RUN $HERE/newEnvironment-13.ts # setup oracles, data sources and price derivation paths - propose
+$RUN $HERE/newEnvironment-13.ts # setup oracles, data sources and price derivation paths - approve
+$RUN $HERE/newEnvironment-13.ts # setup oracles, data sources and price derivation paths - execute
 
 # Phase 4: Factories
 $RUN scripts/fragments/core/factories/deployJoinFactory.ts
 $RUN scripts/fragments/core/factories/deployFYTokenFactory.ts
 $RUN scripts/fragments/core/factories/deployPoolFactory.ts
 
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-17.ts # orchestrate factories - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-17.ts # orchestrate factories - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-17.ts # orchestrate factories - execute
+$RUN $HERE/newEnvironment-17.ts # orchestrate factories - propose
+$RUN $HERE/newEnvironment-17.ts # orchestrate factories - approve
+$RUN $HERE/newEnvironment-17.ts # orchestrate factories - execute
 
 # Phase 5: Core
 $RUN scripts/fragments/core/deployCauldron.ts
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-19.ts # deploy Ladle, using WETH9 from the config file
+$RUN $HERE/newEnvironment-19.ts # deploy Ladle, using WETH9 from the config file
 $RUN scripts/fragments/core/deployWitch.ts
 $RUN scripts/fragments/core/deployWand.ts
 
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-22.ts # orchestrate core - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-22.ts # orchestrate core - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-22.ts # orchestrate core - execute
+$RUN $HERE/newEnvironment-22.ts # orchestrate core - propose
+$RUN $HERE/newEnvironment-22.ts # orchestrate core - approve
+$RUN $HERE/newEnvironment-22.ts # orchestrate core - execute
 
 # Phase 6: Assets
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-23.ts # add assets, deploying joins - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-23.ts # add assets, deploying joins - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-23.ts # add assets, deploying joins - execute
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-24.ts # orchestrate joins, make bases, make ilks - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-24.ts # orchestrate joins, make bases, make ilks - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-24.ts # orchestrate joins, make bases, make ilks - execute
+$RUN $HERE/newEnvironment-23.ts # add assets, deploying joins - propose
+$RUN $HERE/newEnvironment-23.ts # add assets, deploying joins - approve
+$RUN $HERE/newEnvironment-23.ts # add assets, deploying joins - execute
+$RUN $HERE/newEnvironment-24.ts # orchestrate joins, make bases, make ilks - propose
+$RUN $HERE/newEnvironment-24.ts # orchestrate joins, make bases, make ilks - approve
+$RUN $HERE/newEnvironment-24.ts # orchestrate joins, make bases, make ilks - execute
 
 # Phase 7: Series, Pools, Strategies
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-25.ts # add series - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-25.ts # add series - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-25.ts # add series - execute
+$RUN $HERE/newEnvironment-25.ts # add series - propose
+$RUN $HERE/newEnvironment-25.ts # add series - approve
+$RUN $HERE/newEnvironment-25.ts # add series - execute
 
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-26.ts # deploy strategies
+$RUN $HERE/newEnvironment-26.ts # deploy strategies
 
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-27.ts # orchestrate fyToken and strategies, initialize pools and strategies - propose
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-27.ts # orchestrate fyToken and strategies, initialize pools and strategies - approve
-$RUN scripts/governance/newEnvironment/ethereum/newEnvironment-27.ts # orchestrate fyToken and strategies, initialize pools and strategies - execute
+$RUN $HERE/newEnvironment-27.ts # orchestrate fyToken and strategies, initialize pools and strategies - propose
+$RUN $HERE/newEnvironment-27.ts # orchestrate fyToken and strategies, initialize pools and strategies - approve
+$RUN $HERE/newEnvironment-27.ts # orchestrate fyToken and strategies, initialize pools and strategies - execute
