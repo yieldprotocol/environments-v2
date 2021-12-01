8dce3e74a728594b3e65e922e01bc569d1b49fb9
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-12-01 06:24:06 +0000

    feat: deployed LidoWrapHandler to rinkeby

--- scripts/governance/ypp-0010-addStETHWrapper/addStETHWrapper.sh
+++ scripts/governance/ypp-0010-addStETHWrapper/addStETHWrapper.sh
@@ -1,4 +1,8 @@
-npx hardhat run --network kovan scripts/governance/ypp-0010-addStETHWrapper/deployLidoWrapHandler.ts &&
-npx hardhat run --network kovan --no-compile scripts/governance/ypp-0010-addStETHWrapper/addStETHWrapper-2.ts &&
-npx hardhat run --network kovan --no-compile scripts/governance/ypp-0010-addStETHWrapper/addStETHWrapper-2.ts &&
-npx hardhat run --network kovan --no-compile scripts/governance/ypp-0010-addStETHWrapper/addStETHWrapper-2.ts
+set -eux
+RUN="npx hardhat run --network rinkeby"
+HERE=$(dirname $0)
+
+$RUN $HERE/deployLidoWrapHandler.ts &&
+$RUN $HERE/addStETHWrapper-2.ts &&
+$RUN $HERE/addStETHWrapper-2.ts &&
+$RUN $HERE/addStETHWrapper-2.ts
