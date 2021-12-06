3b3425137007ea5c81e7466099ea3b187db3343c
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-12-06 15:24:15 +0000

    feat: update dust

---
+++ scripts/governance/updateDust/updateDust.ts
@@ -0,0 +1,41 @@
+/**
+ * @dev This script updates the dust limits for one or more base/ilk pairs.
+ *
+ * It takes as inputs the governance and protocol address files.
+ */
+
+import { ethers } from 'hardhat'
+
+import {
+  getOwnerOrImpersonate,
+  getOriginalChainId,
+  proposeApproveExecute,
+  getGovernanceProtocolAddresses
+} from '../../../shared/helpers'
+import { updateDustProposal } from '../../fragments/limits/updateDustProposal'
+import { Cauldron, Timelock } from '../../../typechain'
+import { newMin, developer } from './updateDust.config'
+
+;(async () => {
+  const chainId = await getOriginalChainId()
+  const [governance, protocol] = await getGovernanceProtocolAddresses(chainId)
+  let ownerAcc = await getOwnerOrImpersonate(developer.get(chainId) as string)
+
+  // Contract instantiation
+  const cauldron = ((await ethers.getContractAt(
+    'Cauldron',
+    protocol.get('cauldron') as string,
+    ownerAcc
+  )) as unknown) as Cauldron
+
+  const timelock = ((await ethers.getContractAt(
+    'Timelock',
+    governance.get('timelock') as string,
+    ownerAcc
+  )) as unknown) as Timelock
+
+  // Build the proposal
+  const proposal: Array<{ target: string; data: string }> = await updateDustProposal(cauldron, newMin)
+  // Propose, Approve & execute
+  await proposeApproveExecute(timelock, proposal, governance.get('multisig') as string)
+})()
