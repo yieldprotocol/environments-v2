9ce507df1d5e4332b4e099e6e5f58166a485d682
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-12-09 16:04:16 +0000

    feat: import FlashLiquidator

--- contracts/Importer.sol
+++ contracts/Importer.sol
@@ -28,3 +28,5 @@ import "@yield-protocol/utils-v2/contracts/utils/Relay.sol";
 import "@yield-protocol/utils-v2/contracts/utils/Timelock.sol";
 import "@yield-protocol/utils-v2/contracts/utils/EmergencyBrake.sol";
 import "@yield-protocol/strategy-v2/contracts/Strategy.sol";
+import "@yield-protocol/yield-liquidator-v2/contracts/FlashLiquidator.sol";
+import "@yield-protocol/yield-liquidator-v2/contracts/WstethFlashLiquidator.sol";
