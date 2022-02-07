5c9fbffa1649564f8212e27f6fb1bb5ea58c5924
iamsahu <praffulsahu@gmail.com>
2022-02-07 17:58:45 +0530

    fix: witch initialOffer & limit update in single proposal

--- scripts/governance/updateAuctions/updateAuctions.mainnet.config.ts
+++ scripts/governance/updateAuctions/updateAuctions.mainnet.config.ts
@@ -2,29 +2,15 @@ import { ETH, DAI, USDC, WBTC, WSTETH, LINK, ENS, UNI, YVUSDC } from '../../../s
    
 export const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 
-// Input data: ilkId, auctionLine, auctionDust, dec
-export const newLimits: Array<[string, number, number, number]> = [
-  [ETH,    500000000, 10000, 12],
-  [DAI,    1000000,   5000,  18],
-  [USDC,   1000000,   5000,  6],
-  [WBTC,   300000,    2100,  4],
-  [WSTETH, 500000,    10000, 12],
-  [LINK,   1000000,   300,   18],
-  [ENS,    2000000,   300,   18],
-  [YVUSDC, 1000000,   5000,  18],
-  [UNI,    1000000,   100,   18],
-]
-
-
-// Input data: ilkId, initialOffer
-export const newInitialOffer: Array<[string, number]> = [
-  [ETH,    714000],
-  [DAI,    751000],
-  [USDC,   751000],
-  [WBTC,   666000],
-  [WSTETH, 714000],
-  [LINK,   600000],
-  [ENS,    600000],
-  [YVUSDC, 800000],
-  [UNI,    600000],
+// Input data: ilkId, initialOffer, auctionLine, auctionDust, dec
+export const newLimits: Array<[string, number, number, number, number]> = [
+  [ETH,    714000, 325000000, 1000000, 12],
+  [DAI,    751000, 1000000,   5000,    18],
+  [USDC,   751000, 1000000,   5000,    6],
+  [WBTC,   666000, 250000,    1200,    4],
+  [WSTETH, 714000, 325000000, 1000000, 12],
+  [LINK,   600000, 1000000,   300,     18],
+  [ENS,    600000, 2000000,   300,     18],
+  [YVUSDC, 800000, 1000000,   5000,    18],
+  [UNI,    600000, 1000000,   100,     18],
 ]
