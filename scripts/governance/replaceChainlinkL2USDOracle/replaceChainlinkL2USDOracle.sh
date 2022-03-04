f90ca3dd4bd880da7acb60034a9fe99f2921c0b3
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-03-04 12:10:35 +0000

    feat: deployed to arbitrum mainnet

--- scripts/governance/replaceChainlinkL2USDOracle/replaceChainlinkL2USDOracle.sh
+++ scripts/governance/replaceChainlinkL2USDOracle/replaceChainlinkL2USDOracle.sh
@@ -2,8 +2,8 @@
 
 set -eux
 HERE=$(dirname $0)
-export CONF=$PWD/$HERE/replaceChainlinkL2USDOracle.arb_rinkeby.config
-RUN="npx hardhat run --network arb_rinkeby"
+export CONF=$PWD/$HERE/replaceChainlinkL2USDOracle.arb_mainnet.config
+RUN="npx hardhat run --network arb_mainnet"
 
 $RUN $HERE/deployChainlinkL2USDOracle.ts
 $RUN $HERE/setupOracles.ts
