2e4ffaa8df12da6d98db037a3c1b0396d35558ef
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-03-17 15:10:35 +0000

    fix: deploy March maturities

--- scripts/governance/addFCashCollateral/addFCash.mainnet.config.ts
+++ scripts/governance/addFCashCollateral/addFCash.mainnet.config.ts
@@ -13,29 +13,29 @@ export const newPools: Map<string, string> = base_config.newPools
 export const newStrategies: Map<string, string> = base_config.newStrategies
 
 import { USDC, DAI } from '../../../shared/constants'
-import { FUSDC2206, FDAI2206 } from '../../../shared/constants'
-import { FCASH_JUN22, FCASH_DAI, FCASH_USDC } from '../../../shared/constants'
-import { FYUSDC2206, FYDAI2206 } from '../../../shared/constants'
+import { FUSDC2203, FDAI2203 } from '../../../shared/constants'
+import { FCASH_MAR22, FCASH_DAI, FCASH_USDC } from '../../../shared/constants'
+import { FYUSDC2203, FYDAI2203 } from '../../../shared/constants'
 
 export const fCashAddress = '0x1344A36A1B56144C3Bc62E7757377D288fDE0369'
 
 // fcash, notionalId, underlyingId, underlying
 export const notionalSources: Array<[string, string, string, string]> = [
-  [fCashAddress, FDAI2206, DAI, assets.get(DAI) as string],
-  [fCashAddress, FUSDC2206, USDC, assets.get(USDC) as string],
+  [fCashAddress, FDAI2203, DAI, assets.get(DAI) as string],
+  [fCashAddress, FUSDC2203, USDC, assets.get(USDC) as string],
 ]
 
 // Assets for which we will have a Join
 // assetId, fCashId, fCash
 export const assetsToAdd: Array<[string, string, string, number, string]> = [
-  [FDAI2206, fCashAddress, assets.get(DAI) as string, FCASH_JUN22, FCASH_DAI],
-  [FUSDC2206, fCashAddress, assets.get(USDC) as string, FCASH_JUN22, FCASH_USDC],
+  [FDAI2203, fCashAddress, assets.get(DAI) as string, FCASH_MAR22, FCASH_DAI],
+  [FUSDC2203, fCashAddress, assets.get(USDC) as string, FCASH_MAR22, FCASH_USDC],
 ]
 
 // Input data: baseId, ilkId, ratio (1000000 == 100%), line, dust, dec
 export const notionalDebtLimits: Array<[string, string, number, number, number, number]> = [
-  [DAI, FDAI2206, 1100000, 1000000, 5000, 18],
-  [USDC, FUSDC2206, 1100000, 1000000, 5000, 6],
+  [DAI, FDAI2203, 1100000, 1000000, 5000, 18],
+  [USDC, FUSDC2203, 1100000, 1000000, 5000, 6],
 ]
 
 // Input data: ilkId, duration, initialOffer, auctionLine, auctionDust, ilkDec
@@ -44,6 +44,6 @@ export const auctionLimits: Array<[string, number, number, number, number, numbe
 
 // Input data: seriesId, [ilkIds]
 export const seriesIlks: Array<[string, string[]]> = [
-  [FYDAI2206, [FDAI2206]],
-  [FYUSDC2206, [FUSDC2206]],
+  [FYDAI2203, [FDAI2203]],
+  [FYUSDC2203, [FUSDC2203]],
 ]
