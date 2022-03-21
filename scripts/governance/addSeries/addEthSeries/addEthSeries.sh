96de94c2d7a4b7ea926d889d68343ee12498acd7
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-03-21 07:09:45 +0000

    redeployed proposal with wstETH/ETH LTV of 80%

--- scripts/governance/addSeries/addEthSeries/addEthSeries.sh
+++ scripts/governance/addSeries/addEthSeries/addEthSeries.sh
@@ -8,10 +8,10 @@ RUN="npx hardhat run --network mainnet"
 # Add funds to the timelock
 # $RUN $HERE/loadTimelock.ts
 
-$RUN $HERE/deployWrapEtherModule.ts # deploy WrapEtherModule
-$RUN $HERE/../../newEnvironment/deployFYTokens.ts # deploy fyTokens
-$RUN $HERE/../../newEnvironment/deployPools.ts # deploy pools
-$RUN $HERE/../../newEnvironment/deployStrategies.ts # deploy strategies
+# $RUN $HERE/deployWrapEtherModule.ts # deploy WrapEtherModule
+# $RUN $HERE/../../newEnvironment/deployFYTokens.ts # deploy fyTokens
+# $RUN $HERE/../../newEnvironment/deployPools.ts # deploy pools
+# $RUN $HERE/../../newEnvironment/deployStrategies.ts # deploy strategies
 
 $RUN $HERE/addEthSeries.ts
 # $RUN $HERE/addEthSeries.ts
