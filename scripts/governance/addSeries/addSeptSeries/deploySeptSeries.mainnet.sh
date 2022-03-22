1284f75c10b724186dddd728d31a5befc2fe6379
iamsahu <praffulsahu@gmail.com>
2022-03-22 12:43:58 +0530

    fix: cleaned up deploy script

--- scripts/governance/addSeries/addSeptSeries/deploySeptSeries.mainnet.sh
+++ scripts/governance/addSeries/addSeptSeries/deploySeptSeries.mainnet.sh
@@ -7,12 +7,3 @@ RUN="npx hardhat run --network localhost"
 
 $RUN $HERE/../../newEnvironment/deployFYTokens.ts # deploy fyTokens
 $RUN $HERE/../../newEnvironment/deployPools.ts # deploy pools
-
-# $RUN $HERE/loadTimelock.ts
-# $RUN $HERE/advanceToMaturity.ts
-
-# $RUN $HERE/addSeptSeries-3.ts
-# $RUN $HERE/addSeptSeries-3.ts
-# $RUN $HERE/addSeptSeries-3.ts
-
-# $RUN $HERE/debugEndRoll.ts
