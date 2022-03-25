e91c514f1e7554b4477dc6d5663e56960b63035a
iamsahu <praffulsahu@gmail.com>
2022-03-25 15:49:36 +0530

    chore: september series pools & fyTokens deployed

--- scripts/governance/addSeries/addSeptSeries/deploySeptSeries.arbitrum.sh
+++ scripts/governance/addSeries/addSeptSeries/deploySeptSeries.arbitrum.sh
@@ -3,7 +3,7 @@
 set -eux
 HERE=$(dirname $0)
 export CONF=$PWD/$HERE/addSeptSeries.arbitrum.config
-RUN="npx hardhat run --network localhost"
+RUN="npx hardhat run --network arb_mainnet"
 
 $RUN $HERE/../../newEnvironment/deployFYTokens.ts # deploy fyTokens
 $RUN $HERE/../../newEnvironment/deployPools.ts # deploy pools
