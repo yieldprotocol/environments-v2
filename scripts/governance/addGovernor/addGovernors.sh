f4caf1cc824438ce3fadd56ad48037188fb7bb56
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-03-25 14:56:06 +0000

    feat: multisig is governor, alberto is developer

--- scripts/governance/addGovernor/addGovernors.sh
+++ scripts/governance/addGovernor/addGovernors.sh
@@ -2,8 +2,8 @@
 
 set -eux
 HERE=$(dirname $0)
-export CONF=$PWD/$HERE/addGovernors.mainnet.config
-RUN="npx hardhat run --network localhost"
+export CONF=$PWD/$HERE/addGovernors.arb_mainnet.config
+RUN="npx hardhat run --network arb_mainnet"
 
 $RUN $HERE/addGovernors.ts
 $RUN $HERE/addGovernors.ts
