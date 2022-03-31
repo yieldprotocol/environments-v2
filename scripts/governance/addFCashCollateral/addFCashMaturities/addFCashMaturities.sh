e8b3b152f4ce27fc74c71dca27e489c34ef9f877
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-03-31 11:59:16 +0100

    Uncomment the execution script

--- scripts/governance/addFCashCollateral/addFCashMaturities/addFCashMaturities.sh
+++ scripts/governance/addFCashCollateral/addFCashMaturities/addFCashMaturities.sh
@@ -2,11 +2,11 @@
 
 set -eux
 HERE=$(dirname $0)
-RUN="npx hardhat run --network mainnet"
 export CONF=$PWD/$HERE/addFCashMaturities.mainnet.config
+RUN="npx hardhat run --network mainnet"
 
 $RUN $HERE/../deployNotionalJoins.ts # Deploy Joins
 
 $RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - propose
-# $RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - approve
-# $RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - execute
+$RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - approve
+$RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - execute
