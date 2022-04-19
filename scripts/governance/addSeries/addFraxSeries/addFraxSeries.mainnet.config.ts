146e0202796879999d0b55c3f86065f00c811cf9
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-04-19 11:18:19 +0100

    fix: init amounts, add natspec

--- scripts/governance/addSeries/addFraxSeries/addFraxSeries.mainnet.config.ts
+++ scripts/governance/addSeries/addFraxSeries/addFraxSeries.mainnet.config.ts
@@ -50,36 +50,67 @@ export const newStrategies: Map<string, string> = base_config.newStrategies
 export const newJoins: Map<string, string> = readAddressMappingIfExists('newJoins.json')
 
 /// @notice Configuration of the acummulator
-/// @param asset Asset for an acummulator
-/// @param type Type for an acummulator
-/// @param start Initial value for the acummulator
-/// @param increasePerSecond Acummulator multiplier, per second
+/// @param Asset identifier (bytes6 tag)
+/// @param Acummulator type (bytes6 tag)
+/// @param start Initial value for the acummulator (18 decimal fixed point)
+/// @param increasePerSecond Acummulator multiplier, per second (18 decimal fixed point)
 export const rateChiSources: Array<[string, string, string, string]> = [
   [FRAX, RATE, WAD.toString(), '1000000001546067000'],
   [FRAX, CHI, WAD.toString(), WAD.toString()],
 ]
 
-// Assets that will be made into a base
-export const bases: Array<[string, string]> = [[FRAX, newJoins.get(FRAX) as string]]
-
+/// @notice Assets that will be added to the protocol
+/// @param Asset identifier (bytes6 tag)
+/// @param Address for the asset
 export const assetsToAdd: Map<string, string> = new Map([[FRAX, assets.get(FRAX) as string]])
 
+/// @notice Assets that will be made into a base
+/// @param Asset identifier (bytes6 tag)
+/// @param Address for the related Join
+export const bases: Array<[string, string]> = [[FRAX, newJoins.get(FRAX) as string]]
+
+/// @notice Sources that will be added to the Chainlink Oracle
+/// @param Base asset identifier (bytes6 tag)
+/// @param Address for the base asset
+/// @param Quote asset identifier (bytes6 tag)
+/// @param Address for the quote asset
+/// @param Address for the chainlink aggregator
 export const chainlinkSources: Array<[string, string, string, string, string]> = [
   [FRAX, assets.get(FRAX) as string, ETH, assets.get(ETH) as string, '0x14d04Fff8D21bd62987a5cE9ce543d2F1edF5D3E'],
 ]
 
+/// @notice Oracles that will be used for Chi
+/// @param Asset identifier (bytes6 tag)
+/// @param Address for the chi oracle
 export const newChiSources: Array<[string, string]> = [[FRAX, protocol.get(ACCUMULATOR) as string]]
 
+/// @notice Oracles that will be used for Rate
+/// @param Asset identifier (bytes6 tag)
+/// @param Address for the rate oracle
 export const newRateSources: Array<[string, string]> = [[FRAX, protocol.get(ACCUMULATOR) as string]]
 
+/// @notice Sources that will be added to the Composite Oracle
+/// @param Base asset identifier (bytes6 tag)
+/// @param Quote asset identifier (bytes6 tag)
+/// @param Address for the source
 export const compositeSources: Array<[string, string, string]> = [[FRAX, ETH, protocol.get(CHAINLINK) as string]]
 
+/// @notice Paths that will be added to the Composite Oracle
+/// @param Base asset identifier (bytes6 tag)
+/// @param Quote asset identifier (bytes6 tag)
+/// @param Path to traverse (array of bytes6 tags)
 export const newCompositePaths: Array<[string, string, Array<string>]> = [
   [FRAX, ENS, [ETH]],
   [FRAX, WSTETH, [ETH, STETH]],
 ]
 
-// Input data: baseId, ilkId, ratio (1000000 == 100%), line, dust, dec
+/// @notice Configure an asset as an ilk for a base using the Chainlink Oracle
+/// @param Base asset identifier (bytes6 tag)
+/// @param Ilk asset identifier (bytes6 tag)
+/// @param Collateralization ratio as a fixed point number with 6 decimals
+/// @param Debt ceiling, modified by decimals
+/// @param Minimum vault debt, modified by decimals
+/// @param Decimals to append to debt ceiling and minimum vault debt.
 export const newChainlinkLimits: Array<[string, string, number, number, number, number]> = [
   [FRAX, FRAX, 1000000, 5000000, 5000, 18],
   [FRAX, ETH, 1400000, 1000000, 5000, 18],
@@ -93,13 +124,26 @@ export const newChainlinkLimits: Array<[string, string, number, number, number, 
   [ETH, FRAX, 1400000, 1000000, 5000, 12],
 ]
 
-// Input data: baseId, ilkId, ratio (1000000 == 100%), line, dust, dec
+/// @notice Configure an asset as an ilk for a base using the Composite Oracle
+/// @param Base asset identifier (bytes6 tag)
+/// @param Ilk asset identifier (bytes6 tag)
+/// @param Collateralization ratio as a fixed point number with 6 decimals
+/// @param Debt ceiling, modified by decimals
+/// @param Minimum vault debt, modified by decimals
+/// @param Decimals to append to debt ceiling and minimum vault debt.
 export const newCompositeLimits: Array<[string, string, number, number, number, number]> = [
   [FRAX, WSTETH, 1400000, 1000000, 5000, 18],
   [FRAX, ENS, 1670000, 1000000, 5000, 18],
 ]
 
-// seriesId, underlyingId, chiOracleAddress, joinAddress, maturity, name, symbol
+/// @notice Deploy fyToken series
+/// @param series identifier (bytes6 tag)
+/// @param underlying identifier (bytes6 tag)
+/// @param Address for the chi oracle
+/// @param Address for the related Join
+/// @param Maturity in unix time (seconds since Jan 1, 1970)
+/// @param Name for the series
+/// @param Symbol for the series
 export const fyTokenData: Array<[string, string, string, string, number, string, string]> = [
   [
     FYFRAX2206,
@@ -121,7 +165,13 @@ export const fyTokenData: Array<[string, string, string, string, number, string,
   ],
 ]
 
-// seriesId, baseAddress, fyTokenAddress, ts (time stretch), g1 (Sell base to the pool fee), g2 (Sell fyToken to the pool fee)
+/// @notice Deploy YieldSpace pools
+/// @param pool identifier, usually matching the series (bytes6 tag)
+/// @param base address
+/// @param fyToken address
+/// @param time stretch, in 64.64
+/// @param g1, in 64.64
+/// @param g2, in 64.64
 export const poolData: Array<[string, string, string, BigNumber, BigNumber, BigNumber]> = [
   [
     FYFRAX2206,
@@ -141,13 +191,19 @@ export const poolData: Array<[string, string, string, BigNumber, BigNumber, BigN
   ],
 ]
 
-// seriesId, initAmount
+/// @notice Pool initialization parameters
+/// @param pool identifier, usually matching the series (bytes6 tag)
+/// @param base identifier (bytes6 tag)
+/// @param amount of base to initialize pool with
+/// @param amount of fyToken to initialize pool with
 export const poolsInit: Array<[string, string, BigNumber, BigNumber]> = [
-  [FYFRAX2206, FRAX, WAD, ZERO],
-  [FYFRAX2209, FRAX, WAD, ZERO],
+  [FYFRAX2206, FRAX, WAD.mul(100), ZERO],
+  [FYFRAX2209, FRAX, WAD.mul(100), ZERO],
 ]
 
-// seriesId, accepted ilks
+/// @notice Ilks to accept for series
+/// @param series identifier (bytes6 tag)
+/// @param newly accepted ilks (array of bytes6 tags)
 export const seriesIlks: Array<[string, string[]]> = [
   [FYFRAX2206, [FRAX, ETH, DAI, USDC, WBTC, WSTETH, LINK, ENS, UNI]],
   [FYFRAX2209, [FRAX, ETH, DAI, USDC, WBTC, WSTETH, LINK, ENS, UNI]],
@@ -159,20 +215,36 @@ export const seriesIlks: Array<[string, string[]]> = [
   [FYUSDC2209, [FRAX]],
 ]
 
+/// @notice Deploy strategies
+/// @param strategy name
+/// @param strategy identifier (bytes6 tag)
+/// @param Address for the related Join
+/// @param Address for the related asset
 export const strategiesData: Array<[string, string, string, string, string]> = [
   // name, symbol, baseId
   ['Yield Strategy FRAX 6M Mar Sep', YSFRAX6MMS, FRAX, newJoins.get(FRAX) as string, assets.get(FRAX) as string],
   ['Yield Strategy FRAX 6M Jun Dec', YSFRAX6MJD, FRAX, newJoins.get(FRAX) as string, assets.get(FRAX) as string],
 ]
 
-// Input data
+/// @notice Strategy initialization parameters
+/// @param strategy identifier (bytes6 tag)
+/// @param address of initial pool
+/// @param series identifier (bytes6 tag)
+/// @param amount of base to initialize strategy with
 export const strategiesInit: Array<[string, string, string, BigNumber]> = [
   // [strategyId, startPoolAddress, startPoolId, initAmount]
-  [YSFRAX6MMS, newPools.get(FYFRAX2209) as string, FYFRAX2209, WAD],
-  [YSFRAX6MJD, newPools.get(FYFRAX2206) as string, FYFRAX2206, WAD],
+  [YSFRAX6MMS, newPools.get(FYFRAX2209) as string, FYFRAX2209, WAD.mul(100)],
+  [YSFRAX6MJD, newPools.get(FYFRAX2206) as string, FYFRAX2206, WAD.mul(100)],
 ]
 
 // Input data: ilkId, duration, initialOffer, auctionLine, auctionDust, dec
+/// @notice Limits to be used in an auction
+/// @param base identifier (bytes6 tag)
+/// @param duration of auctions in seconds
+/// @param initial percentage of the collateral to be offered (fixed point with 6 decimals)
+/// @param Maximum concurrently auctionable for this asset, modified by decimals
+/// @param Minimum vault debt, modified by decimals
+/// @param Decimals to append to auction ceiling and minimum vault debt.
 export const chainlinkAuctionLimits: Array<[string, number, number, number, number, number]> = [
   [FRAX, 3600, 1000000, 1000000, 5000, 18],
 ]
