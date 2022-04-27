181b4b97b5a594c58b95b2c5b40957d0f900070a
alcueca <alberto@yield.is>
2022-04-27 14:41:20 +0100

    tweak: update dependencies

--- scripts/governance/add/addStEthConverter/addStETHConverter.rinkeby.sh
+++ scripts/governance/add/addStEthConverter/addStETHConverter.rinkeby.sh
@@ -3,7 +3,7 @@ HERE=$(dirname $0)
 export CONF=$PWD/$HERE/addStETHConverter.rinkeby.config
 RUN="npx hardhat run --network rinkeby"
 
-$RUN $HERE/deployStETHConverter.ts &&
-$RUN $HERE/addStETHConverter.ts &&
-$RUN $HERE/addStETHConverter.ts &&
-$RUN $HERE/addStETHConverter.ts
+$RUN $HERE/deployStETHConverter.ts
+# $RUN $HERE/addStETHConverter.ts
+# $RUN $HERE/addStETHConverter.ts
+# $RUN $HERE/addStETHConverter.ts
