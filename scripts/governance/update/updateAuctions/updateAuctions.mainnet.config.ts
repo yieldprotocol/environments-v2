1145958f14de97375f8df5f7ddaaf2e71b1deb79
alcueca <alberto@yield.is>
2022-05-11 06:38:32 +0100

    natspec

--- scripts/governance/update/updateAuctions/updateAuctions.mainnet.config.ts
+++ scripts/governance/update/updateAuctions/updateAuctions.mainnet.config.ts
@@ -2,7 +2,13 @@ import { ETH, DAI, USDC, WBTC, WSTETH, LINK, ENS, UNI, YVUSDC, FRAX } from '../.
 
 export const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 
-// Input data: ilkId, initialOffer, auctionLine, auctionDust, dec
+// Input data: ilkId, duration, initialOffer, auctionLine, auctionDust, dec
+/// @notice Limits to be used in an auction
+/// @param base identifier (bytes6 tag)
+/// @param initial percentage of the collateral to be offered (fixed point with 6 decimals)
+/// @param Maximum concurrently auctionable for this asset, modified by decimals
+/// @param Minimum vault debt, modified by decimals
+/// @param Decimals to append to auction ceiling and minimum vault debt.
 export const newLimits: Array<[string, string, number, number, number]> = [
   [ETH,    '666000000000000000', 32500000000, 1500000, 12],
   [DAI,    '714000000000000000', 100000000,   5000,    18],
