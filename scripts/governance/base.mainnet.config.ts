640c17cefc3c0ecb512b5ff5ece14652fb468f92
alcueca <alberto@yield.is>
2022-06-07 10:35:47 +0100

    feat: corrected USDC whale

--- scripts/governance/base.mainnet.config.ts
+++ scripts/governance/base.mainnet.config.ts
@@ -16,7 +16,7 @@ export const deployer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 export const whales: Map<string, string> = new Map([
   [ETH, '0xd51a44d3fae010294c616388b506acda1bfaae46'],
   [DAI, '0x5d38b4e4783e34e2301a2a36c39a03c45798c4dd'],
-  [USDC, '0xad41bd1cf3fd753017ef5c0da8df31a3074ea1ea'],
+  [USDC, '0xcffad3200574698b78f32232aa9d63eabd290703'],
   [WBTC, '0xd51a44d3fae010294c616388b506acda1bfaae46'],
   [WSTETH, '0x10cd5fbe1b404b7e19ef964b63939907bdaf42e2'],
   [STETH, '0x1982b2f5814301d4e9a8b0201555376e62f82428'],
