288c029b109134fba14e6432d444df30828538c9
David Brailovsky <davidbrai@gmail.com>
2022-06-16 20:55:14 +0200

    add comments to the EToken config file

--- scripts/governance/add/addCollateral/addEulerCollateral/addEToken.mainnet.config.ts
+++ scripts/governance/add/addCollateral/addEulerCollateral/addEToken.mainnet.config.ts
@@ -13,23 +13,45 @@ export const newStrategies: Map<string, string> = base_config.newStrategies
 
 import { DAI, EDAI, FYDAI2209 } from '../../../../../shared/constants'
 
+/// @notice Mapping of an eToken to its contract address
+/// @param eToken id (bytes6 tag)
+/// @param Address of the eToken contract
 export const assets: Map<string, string> = new Map([[EDAI, '0xe025E3ca2bE02316033184551D4d3Aa22024D9DC']])
 
-// underlyingId, eTokenId, eTokenAddress
+/// @notice Oracle sources to be added to ETokenMultiOracle
+/// @param underlying asset id (bytes6 tag)
+/// @param eToken id (bytes 6 tag)
+/// @param Address of the eToken contract to use as the oracle
 export const eulerSources: Array<[string, string, string]> = [[DAI, EDAI, assets.get(EDAI) as string]]
 
-// Assets for which we will have a Join
+/// @notice Assets for which we will have a Join
+/// @param Asset identifier (bytes6 tag)
+/// @param Address for the asset
 export const assetsToAdd: Array<[string, string]> = [[EDAI, assets.get(EDAI) as string]]
 
-// Input data: baseId, ilkId, oracle name, ratio (1000000 == 100%), line, dust, dec
+/// @notice Configure an asset as an ilk for a base using the ETokenMultiOracle
+/// @param Base asset identifier (bytes6 tag)
+/// @param Ilk asset identifier (bytes6 tag)
+/// @param Collateralization ratio as a fixed point number with 6 decimals
+/// @param Debt ceiling, modified by decimals
+/// @param Minimum vault debt, modified by decimals
+/// @param Decimals to append to debt ceiling and minimum vault debt.
 export const debtLimits: Array<[string, string, number, number, number, number]> = [
   [DAI, EDAI, 1250000, 1000000, 5000, 18],
 ]
 
-// Input data: ilkId, duration, initialOffer, auctionLine, auctionDust, ilkDec
+/// @notice Limits to be used in an auction
+/// @param ilk id (bytes6 tag)
+/// @param duration of auctions in seconds
+/// @param initial percentage of the collateral to be offered (fixed point with 6 decimals)
+/// @param Maximum concurrently auctionable for this asset, modified by decimals
+/// @param Minimum vault debt, modified by decimals
+/// @param Decimals to append to auction ceiling and minimum vault debt.
 export const auctionLimits: Array<[string, number, number, number, number, number]> = [
   [EDAI, 3600, 600000, 1000000, 2, 18],
 ]
 
-// Input data: seriesId, [ilkIds]
+/// @notice Ilks to accept for series
+/// @param series identifier (bytes6 tag)
+/// @param newly accepted ilks (array of bytes6 tag)
 export const seriesIlks: Array<[string, string[]]> = [[FYDAI2209, [EDAI]]]
