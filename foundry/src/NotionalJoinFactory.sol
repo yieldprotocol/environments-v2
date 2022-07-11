bc73b34969ffdd7262116637d8348666263f0544
calnix <calnix.289@gmail.com>
2022-07-11 12:07:18 +0800

    Foundry: fCash Wand

---
+++ foundry/src/NotionalJoinFactory.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.14;
+
+import "./NotionalJoin.sol";
+import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
+import {IEmergencyBrake} from '@yield-protocol/utils-v2/contracts/utils/EmergencyBrake.sol';
+
+/// @dev NotionalJoinFactory creates new join instances supporting Notional Finance's fCash tokens.
+/// @author @calnix 
+contract NotionalJoinFactory is AccessControl {
+
+    NotionalJoin[] public njoins;
+    IEmergencyBrake public cloak;
+
+    event JoinCreated(address indexed asset, address join);
+
+    constructor(IEmergencyBrake cloak_){
+        cloak = cloak_;
+    }
+
+    /// @dev Deploys a new notional join using create2
+    /// @param asset Address of the ERC1155 token. (e.g. fDai2203)
+    /// @param underlying Address of the underlying token. (e.g. Dai)
+    /// @param underlyingJoin Address of the underlying join contract. (e.g. Dai join contract)
+    /// @param maturity Maturity of fCash token. (90-day intervals)
+    /// @param currencyId ERC21155 ID of the fCash token
+    /// @param salt Random number of choice
+    /// @return join Deployed notional join address.  
+    function deploy(address asset, address underlying, address underlyingJoin, uint40 maturity, uint16 currencyId, uint256 salt) external auth returns (address) {
+        
+        NotionalJoin njoin = new NotionalJoin{salt: bytes32(salt)}(asset, underlying, underlyingJoin, maturity, currencyId);
+        
+        njoins.push(njoin);
+
+        _orchestrateJoin(address(njoin));
+
+        emit JoinCreated(asset, address(njoin));
+        return address(njoin);
+    }
+    
+    /// @dev Get address of contract to be deployed
+    /// @param bytecode Bytecode of the contract to be deployed (include constructor params)
+    /// @param salt Random number of choice
+    function getAddress(bytes memory bytecode, uint256 salt) public view returns (address){
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(bytecode)));
+
+        // cast last 20 bytes of hash to address
+        return address(uint160(uint256(hash)));
+    }
+
+    /// @dev Get bytecode of contract to be deployed
+    /// @param asset Address of the ERC1155 token. (e.g. fDai2203)
+    /// @param underlying Address of the underlying token. (e.g. Dai)
+    /// @param underlyingJoin Address of the underlying join contract. (e.g. Dai join contract)
+    /// @param maturity Maturity of fCash token. (90-day intervals)
+    /// @param currencyId Maturity of fCash token. (90-day intervals)
+    /// @return bytes Bytecode of notional join to be passed into getAddress()
+    function getByteCode(address asset, address underlying, address underlyingJoin, uint40 maturity, uint16 currencyId) public pure returns (bytes memory) {
+        
+        bytes memory bytecode = type(NotionalJoin).creationCode;
+
+        //append constructor arguments
+        return abi.encodePacked(bytecode, abi.encode(asset, underlying, underlyingJoin, maturity, currencyId));
+    }
+
+    /// @notice Orchestrate the join to grant & revoke the correct permissions
+    /// @param joinAddress Address of the join to be orchestrated
+    function _orchestrateJoin(address joinAddress) internal {
+        AccessControl njoin = AccessControl(joinAddress);
+
+        // grant ROOT to cloak
+        njoin.grantRole(ROOT, address(cloak));
+        
+        // revoke ROOT from NotionalJoinFactory
+        njoin.renounceRole(ROOT, address(this));
+    }
+
+} 
---
+++ foundry/src/fCashWand.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.14;
+import '@yield-protocol/vault-interfaces/src/ICauldronGov.sol';
+import '@yield-protocol/vault-interfaces/src/IOracle.sol';
+import '@yield-protocol/vault-interfaces/src/ILadleGov.sol';
+import '@yield-protocol/vault-interfaces/src/IJoin.sol';
+import '@yield-protocol/utils-v2/contracts/access/AccessControl.sol';
+import '@yield-protocol/utils-v2/contracts/token/IERC20Metadata.sol';
+import {IEmergencyBrake} from '@yield-protocol/utils-v2/contracts/utils/EmergencyBrake.sol';
+
+interface IWitchCustom {
+    /// @dev Function to set the auction limit on the witch
+    /// @param ilkId Id of the ilk
+    /// @param duration the auction duration to calculate liquidation prices
+    /// @param initialOffer the proportion of the collateral that will be sold at auction start
+    /// @param line the maximum collateral that can be auctioned at the same time
+    /// @param dust the minimum collateral that must be left when buying, unless buying all
+    /// @param dec The decimals for maximum and minimum
+    function setIlk(
+        bytes6 ilkId,
+        uint32 duration,
+        uint64 initialOffer,
+        uint96 line,
+        uint24 dust,
+        uint8 dec
+    ) external;
+}
+
+interface IChainlinkMultiOracle {
+    function setSource(
+        bytes6 baseId,
+        IERC20Metadata base,
+        bytes6 quoteId,
+        IERC20Metadata quote,
+        address source
+    ) external;
+}
+
+/// @dev fCash Wand to add Notional.finance ERC1155 tokens as collateral.
+/// @author @calnix
+contract fCashWand is AccessControl {
+    bytes4 public constant JOIN = IJoin.join.selector;
+    bytes4 public constant EXIT = IJoin.exit.selector;
+
+    ICauldronGov public cauldron;
+    ILadleGov public ladle;
+    IEmergencyBrake public cloak;
+    IChainlinkMultiOracle public chainlinkMultiOracle;
+    IWitchCustom public witch;
+
+    struct AuctionLimit {
+        bytes6 ilkId;
+        uint32 duration;
+        uint64 initialOffer;
+        uint96 line;
+        uint24 dust;
+        uint8 dec;
+    }
+
+    struct DebtLimit {
+        bytes6 baseId;
+        bytes6 ilkId;
+        uint32 ratio;
+        uint96 line;
+        uint24 dust;
+        uint8 dec;
+    }
+
+    struct SeriesIlk {
+        bytes6 series;
+        bytes6[] ilkIds;
+    }
+
+    constructor(ICauldronGov cauldron_, ILadleGov ladle_, IWitchCustom witch_, IEmergencyBrake cloak_, IChainlinkMultiOracle chainlinkMultiOracle_) {
+        cauldron = cauldron_;
+        ladle = ladle_;
+        witch = witch_;
+        cloak = cloak_;
+        chainlinkMultiOracle = chainlinkMultiOracle_;
+    }
+
+    /// @notice Function to add fCash as collateral
+    /// @param assetId assetId of the collateral being added
+    /// @param assetAddress address of the collateral
+    /// @param joinAddress address of the join for the asset
+    /// @param auctionLimits auction limits for the asset
+    /// @param debtLimits debt limits for the asset
+    /// @param seriesIlks seriesIlk data to which the asset is to be added
+    function addfCashCollateral(bytes6 assetId, address assetAddress, address joinAddress,
+        AuctionLimit[] calldata auctionLimits,
+        DebtLimit[] calldata debtLimits,
+        SeriesIlk[] calldata seriesIlks
+    ) external auth {
+
+        _addAsset(assetId, assetAddress, joinAddress);
+
+        _makeIlk(joinAddress, auctionLimits, debtLimits);
+
+        _addIlksToSeries(seriesIlks);
+    }
+
+    /// @notice Function to add asset to the cauldron & join to the ladle
+    /// @param assetId Id for the asset being added
+    /// @param assetAddress Address of the asset being added
+    /// @param joinAddress Address of the join for the asset
+    function _addAsset(bytes6 assetId, address assetAddress, address joinAddress) internal {
+        // add asset to cauldron
+        cauldron.addAsset(assetId, assetAddress);
+
+        // Allow Ladle to join and exit on the asset Join
+        bytes4[] memory sigs = new bytes4[](2);
+        sigs[0] = JOIN;
+        sigs[1] = EXIT;
+        AccessControl(joinAddress).grantRoles(sigs, address(ladle));
+        
+        // Register the Join in the Ladle
+        ladle.addJoin(assetId, joinAddress);
+
+        // Register emergency plan to disconnect join from ladle
+        IEmergencyBrake.Permission[] memory permissions = new IEmergencyBrake.Permission[](1);
+        permissions[0] = IEmergencyBrake.Permission(joinAddress, sigs);
+        cloak.plan(address(ladle), permissions);
+    }
+
+    /// @notice Makes the asset into an ilk by setting up auction & debt limits
+    /// @param joinAddress address of the join of the ilk
+    /// @param auctionLimits auction limits for the ilks
+    /// @param debtLimits debt limits for the ilks against bases
+    function _makeIlk(address joinAddress, AuctionLimit[] memory auctionLimits, DebtLimit[] memory debtLimits) internal {
+        // Configure auction limits for the ilk on the witch
+        for (uint256 index = 0; index < auctionLimits.length; index++) {
+            AuctionLimit memory auctionLimit = auctionLimits[index];
+            _updateAuctionLimit(
+                auctionLimit.ilkId,
+                auctionLimit.duration,
+                auctionLimit.initialOffer,
+                auctionLimit.line,
+                auctionLimit.dust,
+                auctionLimit.dec
+            );
+        }
+        // Allow Witch to exit ilk
+        AccessControl join = AccessControl(joinAddress);
+        join.grantRole(EXIT, address(witch));
+
+        // Log a plan to undo the orchestration above in emergencies
+        bytes4[] memory sigs = new bytes4[](1);
+        sigs[0] = EXIT;
+
+        IEmergencyBrake.Permission[] memory permissions = new IEmergencyBrake.Permission[](1);
+        permissions[0] = IEmergencyBrake.Permission(joinAddress, sigs);
+        cloak.plan(address(witch), permissions);
+
+        for (uint256 index = 0; index < debtLimits.length; index++) {
+            DebtLimit memory debtLimit = debtLimits[index];
+            _updateDebtLimit(
+                debtLimit.baseId,
+                debtLimit.ilkId,
+                debtLimit.ratio,
+                debtLimit.line,
+                debtLimit.dust,
+                debtLimit.dec
+            );
+        }
+    }
+
+    /// @dev Function to set the auction limit on the witch
+    /// @param ilkId Id of the ilk
+    /// @param duration the auction duration to calculate liquidation prices
+    /// @param initialOffer the proportion of the collateral that will be sold at auction start
+    /// @param line the maximum collateral that can be auctioned at the same time
+    /// @param dust the minimum collateral that must be left when buying, unless buying all
+    /// @param dec The decimals for maximum and minimum
+    function _updateAuctionLimit(bytes6 ilkId, uint32 duration, uint64 initialOffer, uint96 line, uint24 dust, uint8 dec) internal {
+        witch.setIlk(ilkId, duration, initialOffer, line, dust, dec);
+    }
+
+    /// @dev Set the maximum and minimum debt for an underlying and ilk pair. Can be reset.
+    /// @param baseId asset identifier (bytes6 tag)
+    /// @param ilkId asset identifier (bytes6 tag)
+    /// @param ratio ratio as a fixed point number with 6 decimals
+    /// @param line ceiling, modified by decimals
+    /// @param dust vault debt, modified by decimals
+    /// @param dec to append to debt ceiling and minimum vault debt.
+    function _updateDebtLimit(bytes6 baseId, bytes6 ilkId, uint32 ratio, uint96 line, uint24 dust, uint8 dec) internal {
+        cauldron.setSpotOracle(baseId, ilkId, IOracle(address(chainlinkMultiOracle)), ratio);
+        cauldron.setDebtLimits(baseId, ilkId, line, dust, dec);
+    }
+
+    /// @notice Ilks to accept for series
+    /// @param seriesIlks series & ilks to be added
+    function _addIlksToSeries(SeriesIlk[] calldata seriesIlks) internal {
+        // Add ilks to the series
+        for (uint256 index = 0; index < seriesIlks.length; index++) {
+            SeriesIlk memory seriesIlk = seriesIlks[index];
+            cauldron.addIlks(seriesIlk.series, seriesIlk.ilkIds);
+        }
+    }
+}
