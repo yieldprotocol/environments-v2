1c5a0b6e1180cbe6aa2e5fe280185f71d628a33c
Alberto Cuesta Cañada <alberto@yield.is>
2022-07-25 11:18:54 +0100

    fix: import config

--- scripts/governance/update/updateLimits/updateLimits.mainnet.config.ts
+++ scripts/governance/update/updateLimits/updateLimits.mainnet.config.ts
@@ -1,5 +1,16 @@
 import { ETH, DAI, USDC, WBTC, WSTETH, LINK, ENS, UNI, YVUSDC, FRAX } from '../../../../shared/constants'
 
+import * as base_config from '../../base.mainnet.config'
+
+export const whales: Map<string, string> = base_config.whales
+export const governance: Map<string, string> = base_config.governance
+export const protocol: Map<string, string> = base_config.protocol
+export const assets: Map<string, string> = base_config.assets
+export const joins: Map<string, string> = base_config.joins
+export const newFYTokens: Map<string, string> = base_config.newFYTokens
+export const newPools: Map<string, string> = base_config.newPools
+export const newStrategies: Map<string, string> = base_config.newStrategies
+
 export const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 
 /// @notice Limits to be used in an auction
