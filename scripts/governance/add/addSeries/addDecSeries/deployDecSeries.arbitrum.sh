f79590e1e1e4b009280ac9ab869e03bc4e963178
Richie <devtooligan@gmail.com>
2022-07-29 15:21:27 -0700

    partial work towards arbitrum

--- scripts/governance/add/addSeries/addDecSeries/deployDecSeries.arbitrum.sh
+++ scripts/governance/add/addSeries/addDecSeries/deployDecSeries.arbitrum.sh
@@ -3,7 +3,10 @@
 set -eux
 HERE=$(dirname $0)
 export CONF=$PWD/$HERE/addDecSeries.arbitrum.config
-RUN="npx hardhat run --network arb_mainnet"
+# RUN="npx hardhat run --network arb_mainnet"
+RUN="npx hardhat run --network localhost"
 
+$RUN $HERE/../../../deploy/deployYieldMath.ts # deploy YieldMath
 $RUN $HERE/../../../deploy/deployFYTokens.ts # deploy fyTokens
-$RUN $HERE/../../../deploy/deployPools.ts # deploy pools
+$RUN $HERE/../../../deploy/deployNonTVPools.ts # deploy pools
+$RUN $HERE/../../../deploy/deployRoller.ts # deploy roller
