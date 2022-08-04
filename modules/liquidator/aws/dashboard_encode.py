791ffbc833ecee7d351d7381bfa29f89209c32e0
Alberto Cuesta Cañada <alberto@yield.is>
2022-08-04 10:59:25 +0100

    remove submodules

--- modules/liquidator/aws/dashboard_encode.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse as UP
-import json
-import sys
-
-def main(data):
-    print("widgetDefinition=%s" % UP.quote(json.dumps(json.loads(data))))
-
-if __name__ == "__main__":
-    main(sys.stdin.read())
