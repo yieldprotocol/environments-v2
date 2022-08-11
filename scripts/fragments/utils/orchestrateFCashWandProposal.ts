a75d38dcea7c30258097bcccd403faa3eda7b349
calnix <calnix.289@gmail.com>
2022-08-11 15:02:32 +0800

    orchestrateFCashWandProposal

---
+++ scripts/fragments/utils/orchestrateFCashWandProposal.ts
@@ -0,0 +1,121 @@
+import { ethers } from 'hardhat'
+import { id } from '@yield-protocol/utils-v2'
+import { ROOT } from '../../../shared/constants'
+
+import { Cauldron, NotionalMultiOracle, EmergencyBrake, Join, Ladle, Timelock, Witch } from '../../../typechain'
+import { FCashWand } from '../../../typechain'
+
+const { protocol, governance } = require(process.env.CONF as string)
+export const orchestrateFCashWandProposal = async (
+  ownerAcc: any,
+  deployer: string,
+  timelock: Timelock,
+  join: Join
+): Promise<Array<{ target: string; data: string }>> => {
+  let proposal: Array<{ target: string; data: string }> = []
+
+  const fCashWand = (await ethers.getContractAt(
+    'FCashWand',
+    protocol.get('fCashWand') as string,
+    ownerAcc
+  )) as FCashWand
+
+  const cauldron = (await ethers.getContractAt('Cauldron', protocol.get('cauldron') as string, ownerAcc)) as Cauldron
+
+  const ladle = (await ethers.getContractAt('Ladle', protocol.get('ladle') as string, ownerAcc)) as Ladle
+
+  const spotOracle = (await ethers.getContractAt(
+    'NotionalMultiOracle',
+    protocol.get('notionalMultiOracle') as string,
+    ownerAcc
+  )) as NotionalMultiOracle
+
+  const cloak = (await ethers.getContractAt(
+    'EmergencyBrake',
+    governance.get('cloak') as string,
+    ownerAcc
+  )) as unknown as EmergencyBrake
+
+  const witch = (await ethers.getContractAt('Witch', protocol.get('witch') as string, ownerAcc)) as unknown as Witch
+
+  proposal.push({
+    target: fCashWand.address,
+    data: fCashWand.interface.encodeFunctionData('grantRole', [
+      id(fCashWand.interface, 'addfCashCollateral(bytes6,address,bytes6,bytes6)'),
+      timelock.address,
+    ]),
+  })
+
+  proposal.push({
+    target: fCashWand.address,
+    data: fCashWand.interface.encodeFunctionData('revokeRole', [ROOT, deployer]),
+  })
+
+  // proposal.push({
+  //   target: join.address,
+  //   data: join.interface.encodeFunctionData('grantRole', [
+  //     id(join.interface, 'grantRoles(bytes4[],address)'),
+  //     collateralWand.address,
+  //   ]),
+  // })
+
+  // proposal.push({
+  //   target: join.address,
+  //   data: join.interface.encodeFunctionData('grantRole', ['0x00000000', collateralWand.address]),
+  // })
+
+  proposal.push({
+    target: cauldron.address,
+    data: cauldron.interface.encodeFunctionData('grantRoles', [
+      [
+        id(cauldron.interface, 'addAsset(bytes6,address)'),
+        id(cauldron.interface, 'addIlks(bytes6,bytes6[])'),
+        id(cauldron.interface, 'setSpotOracle(bytes6,bytes6,address,uint32)'),
+        id(cauldron.interface, 'setDebtLimits(bytes6,bytes6,uint96,uint24,uint8)'),
+      ],
+      fCashWand.address,
+    ]),
+  })
+
+  proposal.push({
+    target: ladle.address,
+    data: ladle.interface.encodeFunctionData('grantRole', [
+      id(ladle.interface, 'addJoin(bytes6,address'),
+      fCashWand.address,
+    ]),
+  })
+
+  proposal.push({
+    target: spotOracle.address,
+    data: spotOracle.interface.encodeFunctionData('grantRole', [
+      id(spotOracle.interface, 'setSource(bytes6,address,bytes6,address,address)'),
+      fCashWand.address,
+    ]),
+  })
+
+  proposal.push({
+    target: cloak.address,
+    data: cloak.interface.encodeFunctionData('grantRole', [
+      id(cloak.interface, 'plan(address,(address,bytes4[])[])'),
+      fCashWand.address,
+    ]),
+  })
+
+  proposal.push({
+    target: cloak.address,
+    data: cloak.interface.encodeFunctionData('grantRole', ['0x00000000', fCashWand.address]),
+  })
+
+  proposal.push({
+    target: witch.address,
+    data: witch.interface.encodeFunctionData('grantRoles', [
+      [
+        id(witch.interface, 'point(bytes32,address)'),
+        id(witch.interface, 'setIlk(bytes6,uint32,uint64,uint96,uint24,uint8)'),
+      ],
+      fCashWand.address,
+    ]),
+  })
+
+  return proposal
+}
