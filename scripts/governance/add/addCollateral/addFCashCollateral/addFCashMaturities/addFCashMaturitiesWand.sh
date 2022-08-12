da1167ace531e2ee147709f6aa3ef776206a34d8
calnix <calnix.289@gmail.com>
2022-08-12 14:13:54 +0800

    addFCashMaturities.ts

--- scripts/governance/add/addCollateral/addFCashCollateral/addFCashMaturities/addFCashMaturitiesWand.sh
+++ scripts/governance/add/addCollateral/addFCashCollateral/addFCashMaturities/addFCashMaturitiesWand.sh
@@ -2,7 +2,7 @@
 
 set -eux
 HERE=$(dirname $0)
-export CONF=$PWD/$HERE/addDecSeries.mainnet.config
+#export CONF=$PWD/$HERE/addDecSeries.mainnet.config
 RUN="npx hardhat run --network mainnet"
 
 $RUN $HERE/addFCashMaturitiesWand.ts
