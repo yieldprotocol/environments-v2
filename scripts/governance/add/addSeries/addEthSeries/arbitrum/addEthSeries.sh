216cd46c714b1212b1d321657408f95f708eb483
iamsahu <praffulsahu@gmail.com>
2022-08-17 18:22:13 +0200

    chore: decorated config file

--- scripts/governance/add/addSeries/addEthSeries/arbitrum/addEthSeries.sh
+++ scripts/governance/add/addSeries/addEthSeries/arbitrum/addEthSeries.sh
@@ -3,16 +3,16 @@
 set -eux
 HERE=$(dirname $0)
 export CONF=$PWD/$HERE/addEthSeries.arb_mainnet.config
-RUN="npx hardhat run --network localhost"
+RUN="npx hardhat run --network arb_mainnet"
 
 # Add funds to the timelock
 # $RUN $HERE/../loadTimelock.ts
 
-# $RUN $HERE/../../../../redeploy/deployFYTokens.ts # deploy fyTokens
-# $RUN $HERE/../../../../redeploy/deployPools.ts # deploy pools
-# $RUN $HERE/../../../../redeploy/deployStrategies.ts # deploy strategies
+$RUN $HERE/../../../../redeploy/deployFYTokens.ts # deploy fyTokens
+$RUN $HERE/../../../../redeploy/deployPools.ts # deploy pools
+$RUN $HERE/../../../../redeploy/deployStrategies.ts # deploy strategies
 
 # $RUN $HERE/addEthSeries.ts
 # $RUN $HERE/addEthSeries.ts
 # $RUN $HERE/addEthSeries.ts
-$RUN $HERE/addEthSeries.test.ts
+# $RUN $HERE/addEthSeries.test.ts
