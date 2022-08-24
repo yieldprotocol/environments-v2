a637dc7e727a8b17cae1d2adeec93b5c9d81a087
Egill Hreinsson <egillh210@gmail.com>
2022-08-24 15:23:26 +0100

    add ETH series to config

--- scripts/governance/contango/arbitrum/contango.arb_mainnet.config.ts
+++ scripts/governance/contango/arbitrum/contango.arb_mainnet.config.ts
@@ -13,24 +13,32 @@ export const fyTokens: Map<string, string> = base_config.fyTokens
 export const pools: Map<string, string> = base_config.pools
 export const wethAddress = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
 
-import { USDC, DAI, FYDAI2209, FYUSDC2209, CHAINLINKUSD, IDENTITY } from '../../../../shared/constants' // Note we use the series id as the asset id
+import { USDC, DAI, FYDAI2209, FYUSDC2209, CHAINLINKUSD, IDENTITY, ETH, FYETH2209 } from '../../../../shared/constants' // Note we use the series id as the asset id
 
 // Assets that will be made into a base
 export const bases: Array<[string, string]> = [
   [DAI, joins.get(DAI) as string],
   [USDC, joins.get(USDC) as string],
+  [ETH, joins.get(ETH) as string],
 ]
 
 // Input data: baseId, quoteId, oracle name
 export const compositeSources: Array<[string, string, string]> = [
   [FYDAI2209, DAI, IDENTITY],
   [FYUSDC2209, USDC, IDENTITY],
+  [FYETH2209, ETH, IDENTITY],
   [DAI, USDC, CHAINLINKUSD],
+  [ETH, USDC, CHAINLINKUSD],
+  [ETH, DAI, CHAINLINKUSD],
 ]
 // Input data: assetId, assetId, [intermediate assetId]
 export const compositePaths: Array<[string, string, Array<string>]> = [
   [DAI, FYUSDC2209, [USDC]],
   [USDC, FYDAI2209, [DAI]],
+  [DAI, FYETH2209, [ETH]],
+  [USDC, FYETH2209, [ETH]],
+  [ETH, FYDAI2209, [DAI]],
+  [ETH, FYUSDC2209, [USDC]],
 ]
 
 /// @notice Assets that will be added to the protocol
@@ -40,8 +48,10 @@ export const compositePaths: Array<[string, string, Array<string>]> = [
 export const assetsToAdd: Array<[string, string, string]> = [
   [DAI, assets.get(DAI) as string, joins.get(DAI) as string],
   [USDC, assets.get(USDC) as string, joins.get(USDC) as string],
+  [ETH, assets.get(ETH) as string, joins.get(ETH) as string],
   [FYDAI2209, fyTokens.get(FYDAI2209) as string, newJoins.get(FYDAI2209) as string],
   [FYUSDC2209, fyTokens.get(FYUSDC2209) as string, newJoins.get(FYUSDC2209) as string],
+  [FYETH2209, fyTokens.get(FYETH2209) as string, newJoins.get(FYETH2209) as string],
 ]
 
 // Input data: baseId, ilkId, ratio (1000000 == 100%), line, dust, dec
@@ -54,7 +64,11 @@ export const assetsToAdd: Array<[string, string, string]> = [
 /// @param Decimals to add to maximum protocol debt, and minimum vault debt.
 export const fyTokenDebtLimits: Array<[string, string, number, number, number, number]> = [
   [DAI, FYUSDC2209, 1100000, 10000, 100, 18], // dai collateralized with fyUsdc
+  [DAI, FYETH2209, 1400000, 10000, 100, 18], // dai collateralized with fyEth
   [USDC, FYDAI2209, 1100000, 10000, 100, 6], // usdc collateralized with fyDai
+  [USDC, FYETH2209, 1400000, 10000, 100, 6], // usdc collateralized with fyETH
+  [ETH, FYUSDC2209, 1400000, 10000000, 25000, 12], // eth collateralized with fyUsdc
+  [ETH, FYDAI2209, 1400000, 10000000, 25000, 12], // eth collateralized with fyDai
 ]
 
 // Input data: ilkId, duration, initialOffer, auctionLine, auctionDust, ilkDec
@@ -66,6 +80,7 @@ export const auctionLimits: Array<[string, number, number, number, number, numbe
 /// @param Base asset identifier (bytes6 tag)
 /// @param Array of collateral asset identifiers (bytes6 tag array)
 export const seriesIlks: Array<[string, string[]]> = [
-  [FYDAI2209, [FYUSDC2209]],
-  [FYUSDC2209, [FYDAI2209]],
+  [FYDAI2209, [FYUSDC2209, FYETH2209]],
+  [FYUSDC2209, [FYDAI2209, FYETH2209]],
+  [FYETH2209, [FYUSDC2209, FYDAI2209]],
 ]
