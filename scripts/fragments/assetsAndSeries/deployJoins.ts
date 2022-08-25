88d1ddd90507babd6952caf194161a0bae310007
Egill Hreinsson <egillh210@gmail.com>
2022-08-25 15:31:25 +0100

    fix deployJoins script

--- scripts/fragments/assetsAndSeries/deployJoins.ts
+++ scripts/fragments/assetsAndSeries/deployJoins.ts
@@ -1,8 +1,8 @@
-import { waffle } from 'hardhat'
-import { ROOT } from '../../../shared/constants'
+import { ethers, waffle } from 'hardhat'
+import { ROOT, ZERO_ADDRESS } from '../../../shared/constants'
 import { verify } from '../../../shared/helpers'
 import JoinArtifact from '../../../artifacts/@yield-protocol/vault-v2/contracts/Join.sol/Join.json'
-import { Join, Timelock } from '../../../typechain'
+import { Join, Ladle, Timelock } from '../../../typechain'
 
 const { deployContract } = waffle
 
@@ -13,22 +13,26 @@ const { deployContract } = waffle
 export const deployJoins = async (
   ownerAcc: any,
   timelock: Timelock,
+  ladle: Ladle,
   joinData: Array<[string, string]>
 ): Promise<Map<string, Join>> => {
   let joins: Map<string, Join> = new Map()
   for (let [assetId, assetAddress] of joinData) {
-    let join: Join
-    join = (await deployContract(ownerAcc, JoinArtifact, [assetAddress])) as Join
-    console.log(`Join deployed at ${join.address} for ${assetAddress}`)
-    verify(join.address, [assetAddress])
-
-    if (!(await join.hasRole(ROOT, timelock.address))) {
-      await join.grantRole(ROOT, timelock.address)
-      console.log(`join.grantRoles(ROOT, timelock)`)
-      while (!(await join.hasRole(ROOT, timelock.address))) {}
+    const joinDoesNotExist = (await ladle.joins(assetId)) === ZERO_ADDRESS
+    if (joinDoesNotExist) {
+      const join = await (await ethers.getContractFactory('Join', ownerAcc)).deploy(assetAddress)
+      await join.deployed()
+      console.log(`Join deployed at ${join.address} for ${assetAddress}`)
+      verify(join.address, [assetAddress])
+      if (!(await join.hasRole(ROOT, timelock.address))) {
+        await join.grantRole(ROOT, timelock.address)
+        console.log(`join.grantRoles(ROOT, timelock)`)
+        while (!(await join.hasRole(ROOT, timelock.address))) {}
+      }
+      joins.set(assetId, join)
+    } else {
+      console.log(`Join for assetId: ${assetId} already exists at: ${assetAddress}`)
     }
-
-    joins.set(assetId, join)
   }
 
   return joins
