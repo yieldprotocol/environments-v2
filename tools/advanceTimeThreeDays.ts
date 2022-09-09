918ed3013c070c2f226fb401670f2f0e37f7328b
Alberto Cuesta Cañada <alberto@yield.is>
2022-09-09 22:08:37 +0100

    fix: no need for this one

--- tools/advanceTimeThreeDays.ts
+++
@@ -1,8 +0,0 @@
-import { advanceTime } from '../shared/helpers'
-
-/**
- * @dev This script advances time three days to the future so that an approved proposal can be executed
- */
-;(async () => {
-  await advanceTime(3 * 24 * 60 * 60)
-})()
