81ef5e7fcc35da75a34c65171e0c43b2acf0ef7c
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-09-22 23:03:57 -0500

    chore: partition constants

---
+++ shared/data.ts
@@ -0,0 +1,8 @@
+import { ilkIds, seriesIds } from './constants'
+
+const ilksJSON = JSON.stringify(ilkIds)
+const seriesJSON = JSON.stringify(seriesIds)
+
+console.log(ilksJSON)
+console.log(seriesJSON)
+console.log(ilkIds)
