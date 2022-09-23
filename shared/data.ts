6daa9e0082e21e304bd9bb3d42b24f56df0f9af4
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-09-23 14:30:23 -0500

    fix: get data into json file

---
+++ shared/data.ts
@@ -0,0 +1,9 @@
+// import { base, ilkIds, seriesIds } from "./constants";
+import { assets } from '../scripts/governance/base.mainnet.config'
+
+// const baseJSON = JSON.stringify(base);
+const assetsJSON = JSON.stringify(assets)
+// const ilksJSON = JSON.stringify(ilkIds);
+// const seriesJSON = JSON.stringify(seriesIds);
+
+console.log(assetsJSON)
