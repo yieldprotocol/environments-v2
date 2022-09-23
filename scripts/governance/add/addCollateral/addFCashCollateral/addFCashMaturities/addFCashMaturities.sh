c3ac5e1cc135437516974988b53588678c0c2027
Alberto Cuesta Cañada <alberto@yield.is>
2022-09-23 15:35:55 +0100

    uncomment deployment script

--- scripts/governance/add/addCollateral/addFCashCollateral/addFCashMaturities/addFCashMaturities.sh
+++ scripts/governance/add/addCollateral/addFCashCollateral/addFCashMaturities/addFCashMaturities.sh
@@ -5,9 +5,9 @@ HERE=$(dirname $0)
 export CONF=$PWD/$HERE/addFCashDecMaturities.mainnet.config
 RUN="npx hardhat run --network localhost"
 
-# $RUN $HERE/../deployNotionalJoins.ts # Deploy Joins
+$RUN $HERE/../deployNotionalJoins.ts # Deploy Joins
 
 $RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - propose
-# $RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - approve
-# $RUN $HERE/advanceTimeThreeDays.ts # AdvanceTime
-# $RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - execute
+$RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - approve
+$RUN $HERE/advanceTimeThreeDays.ts # AdvanceTime
+$RUN $HERE/addFCashMaturities.ts # Orchestrate Joins, add assets, make ilks, add ilks to series - execute
