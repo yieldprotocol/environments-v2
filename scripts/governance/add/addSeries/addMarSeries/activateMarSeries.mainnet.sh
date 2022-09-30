e703d6be6ae04fc762356423ccd1743977ffe5c6
Alberto Cuesta Cañada <alberto@yield.is>
2022-09-30 19:03:32 +0100

    info: refreshed join balances

--- scripts/governance/add/addSeries/addMarSeries/activateMarSeries.mainnet.sh
+++ scripts/governance/add/addSeries/addMarSeries/activateMarSeries.mainnet.sh
@@ -4,11 +4,13 @@ set -eux
 HERE=$(dirname $0)
 export CONF=$PWD/$HERE/addMarSeries.mainnet.config
 # RUN="npx hardhat run --network mainnet"
-RUN="npx hardhat run --network tenderly"
+RUN="npx hardhat run --network localhost"
 
-$RUN $HERE/loadTimelock.ts
-$RUN $HERE/advanceTimeToMaturity.ts
- 
-$RUN $HERE/activateMarSeries.ts
-$RUN $HERE/activateMarSeries.ts
-$RUN $HERE/activateMarSeries.ts
+$RUN $HERE/poolRollBalances.ts
+
+# $RUN $HERE/loadTimelock.ts
+# $RUN $HERE/advanceTimeToMaturity.ts
+#  
+# $RUN $HERE/activateMarSeries.ts
+# $RUN $HERE/activateMarSeries.ts
+# $RUN $HERE/activateMarSeries.ts
