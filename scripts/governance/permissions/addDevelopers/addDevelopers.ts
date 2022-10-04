34e78c092a936232ea0ff002bbd21c7380217807
Alberto Cuesta Cañada <alberto@yield.is>
2022-10-04 11:24:46 +0100

    feat: added Bruno as a dev

--- scripts/governance/permissions/addDevelopers/addDevelopers.ts
+++ scripts/governance/permissions/addDevelopers/addDevelopers.ts
@@ -1,33 +1,19 @@
 import { ethers } from 'hardhat'
-import { readAddressMappingIfExists, getOwnerOrImpersonate, proposeApproveExecute } from '../../../../shared/helpers'
+import { proposeApproveExecute } from '../../../../shared/helpers'
 
 import { grantDevelopersProposal } from '../../../fragments/permissions/grantDevelopersProposal'
-import { Timelock, EmergencyBrake } from '../../../../typechain'
 
-const { newDevelopers, developerToImpersonate } = require(process.env.CONF as string)
+const { governance, newDevelopers, developer } = require(process.env.CONF as string)
 
 /**
  * @dev This script gives developer privileges to one or more accounts.
  */
 ;(async () => {
-  let ownerAcc = await getOwnerOrImpersonate(developerToImpersonate as string)
-
-  const governance = readAddressMappingIfExists('governance.json')
-
-  const timelock = (await ethers.getContractAt(
-    'Timelock',
-    governance.get('timelock') as string,
-    ownerAcc
-  )) as unknown as Timelock
-
-  const cloak = (await ethers.getContractAt(
-    'EmergencyBrake',
-    governance.get('cloak') as string,
-    ownerAcc
-  )) as unknown as EmergencyBrake
+  const timelock = await ethers.getContractAt('Timelock', governance.get('timelock') as string)
+  const cloak = await ethers.getContractAt('EmergencyBrake', governance.get('cloak') as string)
 
   let proposal: Array<{ target: string; data: string }> = []
   proposal = proposal.concat(await grantDevelopersProposal(timelock, cloak, newDevelopers))
 
-  await proposeApproveExecute(timelock, proposal, governance.get('multisig') as string)
+  await proposeApproveExecute(timelock, proposal, governance.get('multisig') as string, developer)
 })()
