5b24bab0fc934c7f634123b926bc621e2a03ebfe
marcomariscal <marco.a.mariscal@gmail.com>
2022-10-07 08:30:13 -0700

    fix: remove the use of decimals

--- scripts/governance/emergency/restoreStrategies/sendTokens.arbitrum.config.ts
+++ scripts/governance/emergency/restoreStrategies/sendTokens.arbitrum.config.ts
@@ -1,7 +1,5 @@
 import { BigNumber } from 'ethers'
-import { ethers } from 'hardhat'
 import { DAI, USDC } from '../../../../shared/constants'
-import { ERC20__factory } from '../../../../typechain'
 import * as base_config from '../../base.arb_mainnet.config'
 
 export const governance: Map<string, string> = base_config.governance
@@ -11,20 +9,17 @@ export const developer = '0x1662BbbDdA3fb169f10C495AE27596Af7f8fD3E1'
 export const devFundsRecipient = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 
 const daiAddr = assets.get(DAI) as string
-export const daiToSend = '46987.504556272699004437' // amount recovered from pool
+export const daiToSend = '46987504556272699004437' // 46987.504556272699004437 amount recovered from pool in dai decimals
 
 const usdcAddr = assets.get(USDC) as string
-export const usdcToSend = '159611.601407' // amount recovered from pool
+export const usdcToSend = '159611601407' // 159611.601407 amount recovered from pool in usdc decimals
 
 // Gather the token send data to use in the proposal
 export const sendData = async () => {
-  const signer = ethers.provider.getSigner()
-
   let data: Array<[string, string, BigNumber]> = [] // [token address, destination address, amount]
 
   for (const tokenAddr of [daiAddr, usdcAddr]) {
-    const token = ERC20__factory.connect(tokenAddr, signer)
-    const amount = ethers.utils.parseUnits(tokenAddr === daiAddr ? daiToSend : usdcToSend, await token.decimals())
+    const amount = BigNumber.from(tokenAddr === daiAddr ? daiToSend : usdcToSend)
     data.push([tokenAddr, devFundsRecipient, amount])
   }
 
