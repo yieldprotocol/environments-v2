f89868e01f98be8f0c7058771a15d2f60f4ffb4d
alcueca <alberto@yield.is>
2022-10-11 11:48:29 +0100

    feat: add joinRollDrain

--- tools/joinRollDrain.ts
+++ tools/joinRollDrain.ts
@@ -1,20 +1,16 @@
 import { ethers } from 'hardhat'
 
-const pools = [
-  "0x6BaC09a67Ed1e1f42c29563847F77c28ec3a04FC",
-  "0xf5Fd5A9Db9CcCc6dc9f5EF1be3A859C39983577C",
-  "0xc3348D8449d13C364479B1F114bcf5B73DFc0dc6",
-  "0x4b32C37Be5949e77ba3726E863a030BD77942A97",
-]
+const { pools } = require(process.env.CONF as string)
+
 
 ;(async () => {
-  for (let poolAddress of pools) {
+  for (let [seriesId, poolAddress] of pools) {
     const pool = (await ethers.getContractAt('Pool', poolAddress))
     const base = (await ethers.getContractAt('ERC20', await pool.base()))
     const fyToken = (await ethers.getContractAt('FYToken', await pool.fyToken()))
     const join = (await ethers.getContractAt('Join', await fyToken.join()))
   
-    console.log(`${await base.symbol()}`)
+    console.log(`${seriesId}: ${await base.symbol()} ${await fyToken.maturity()}`)
     console.log(`Pool: ${pool.address}`)
     console.log(`Join: ${join.address}`)
     const poolRedeemableFYToken = await fyToken.balanceOf(pool.address)
