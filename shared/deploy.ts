293ed73c663530bfd3869b1ae52de9897e5743f7
alcueca <alberto@yield.is>
2022-10-21 10:35:26 +0100

    removed functions

--- shared/deploy.ts
+++ shared/deploy.ts
@@ -10,45 +10,6 @@ import { ContractDeployment } from '../scripts/governance/confTypes'
 const { deployer, governance, contractDeployments } = require(process.env.CONF as string)
 
 /**
- * @dev Deploy a contract, verifies it, stores it in a database, and gives ROOT to the Timelock
- * The Timelock gets ROOT access.
- */
-
-// NOTE:
-// due to 'ethers.getContractFactory' having multiple overloads, it is not possible to
-// generify the fuction to take the parameters and infer the return type (<T extndends Parameters<typeof ethers.getContractFactory>>)
-// Therefore, the best we can do is to take the awaited factory as a parameter, and let TS infer the correct params
-// of deploy(). Due to this, we don't have visibility of the libs, meaning the verify() on line 34 won't work if there are libs
-// The easy way to solve that would be to have 2 overloads of this deploy() function, where in one impl you take the libs as explicit arguments.
-// See below (and maybe refactor these two to extract common logic :) )
-export const deploy = async <Factory extends Awaited<ReturnType<typeof ethers.getContractFactory>>>(
-  name: string, // The unique name to the contract
-  contractFactory: Factory, // The path to the .sol file to deploy
-  ...args: Parameters<typeof contractFactory['deploy']>
-): Promise<Contract> => {
-  const deployment: Contract = await contractFactory.deploy(...args)
-
-  await deployment.deployed()
-  console.log(`${name} deployed at ${deployment.address}`)
-
-  return deployment
-}
-
-export const deployWithLibs = async <Factory extends Awaited<ReturnType<typeof ethers.getContractFactory>>>(
-  name: string, // The unique name to the contract
-  libs: FactoryOptions['libraries'],
-  contractFactory: Factory, // The path to the .sol file to deploy
-  ...args: Parameters<typeof contractFactory['deploy']>
-): Promise<Contract> => {
-  const deployment: Contract = await contractFactory.deploy(...args)
-
-  await deployment.deployed()
-  console.log(`${name} deployed at ${deployment.address}`)
-
-  return deployment
-}
-
-/**
  * @dev This script deploys contracts as defined in a proposal config file containing a contractDeployments:ContractDeployment[] export.
  */
 ;(async () => {
@@ -61,15 +22,16 @@ export const deployWithLibs = async <Factory extends Awaited<ReturnType<typeof e
     const deployedAddress = readAddressMappingIfExists(contractToDeploy.addressFile).get(contractToDeploy.name)
     let deployedContract: Contract
     if (deployedAddress === undefined) {
-      deployedContract = await deploy(
-        contractToDeploy.name,
-        await ethers.getContractFactory(contractToDeploy.contract, deployerAcc),
-        ...contractToDeploy.args
-      )
+      const contractFactory = await ethers.getContractFactory(contractToDeploy.contract, deployerAcc)
+
+      deployedContract = await contractFactory.deploy(...contractToDeploy.args)
+
+      await deployedContract.deployed()
+      console.log(`${contractToDeploy.name} deployed at ${deployedContract.address}`)
 
       const addressMap = readAddressMappingIfExists(contractToDeploy.addressFile)
       addressMap.set(contractToDeploy.name, deployedContract.address)
-      writeAddressMap(contractToDeploy.addressFile, addressMap)
+      // writeAddressMap(contractToDeploy.addressFile, addressMap)
 
       verify(contractToDeploy.name, deployedContract, contractToDeploy.args, contractToDeploy.libs)
 
