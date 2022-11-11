06da0a9bacb604f50e7dadc214b470f6f9866e31
alcueca <alberto@yield.is>
2022-11-11 13:29:29 +0000

    feat: complete Witch v2 migration

--- scripts/governance/replace/witchV2/completeWitchV2.mainnet.config.ts
+++ scripts/governance/replace/witchV2/completeWitchV2.mainnet.config.ts
@@ -26,19 +26,10 @@ import {
   YVUSDC,
   UNI,
   FRAX,
-  FYETH2212,
   FYDAI2212,
-  FYUSDC2212,
-  FYFRAX2212,
-  FYETH2303,
   FYDAI2303,
-  FYUSDC2303,
-  FYFRAX2303,
-  CAULDRON,
-  LADLE,
-  WITCH,
 } from '../../../../shared/constants'
-import { AuctionLineAndLimit, ContractDeployment } from '../../confTypes' // Note we use the series id as the asset id
+import { AuctionLineAndLimit } from '../../confTypes' // Note we use the series id as the asset id
 
 // ----- proposal parameters -----
 
@@ -283,19 +274,18 @@ export const v2Limits: AuctionLineAndLimit[] = [
 
 /// @notice Limits to be used in an auction
 /// @param base identifier (bytes6 tag)
-/// @param initial percentage of the collateral to be offered (fixed point with 18 decimals)
 /// @param Maximum concurrently auctionable for this asset, modified by decimals
 /// @param Minimum vault debt, modified by decimals
 /// @param Decimals to append to auction ceiling and minimum vault debt.
-export const v1Limits: Array<[string, string, number, number, number]> = [
-  [ETH, '720000000000000000', 0, 1500000, 12], // DAI/ETH is 72% LTV
-  [DAI, '760000000000000000', 0, 5000, 18], // USDC/DAI is 76% LTV
-  [USDC, '760000000000000000', 0, 5000, 6], // DAI/USDC is 76% LTV
-  [WBTC, '670000000000000000', 0, 1200, 4], // ETH/WBTC is 67% LTV
-  [WSTETH, '800000000000000000', 0, 1500000, 12], // ETH/WSTETH is 80% LTV
-  [LINK, '670000000000000000', 0, 300, 18], // ETH/LINK is 67% LTV
-  [ENS, '670000000000000000', 0, 250, 18], // ETH/ENS is 67% LTV
-  [YVUSDC, '800000000000000000', 0, 5000, 6], // USDC/YVUSDC is 80% LTV
-  [UNI, '670000000000000000', 0, 400, 18], // ETH/UNI is 67% LTV
-  [FRAX, '910000000000000000', 0, 400, 18], // DAI/FRAX is 91% LTV
+export const v1Limits: Array<[string, number, number, number]> = [
+  [ETH, 0, 1500000, 12], // DAI/ETH is 72% LTV
+  [DAI, 0, 5000, 18], // USDC/DAI is 76% LTV
+  [USDC, 0, 5000, 6], // DAI/USDC is 76% LTV
+  [WBTC, 0, 1200, 4], // ETH/WBTC is 67% LTV
+  [WSTETH, 0, 1500000, 12], // ETH/WSTETH is 80% LTV
+  [LINK, 0, 300, 18], // ETH/LINK is 67% LTV
+  [ENS, 0, 250, 18], // ETH/ENS is 67% LTV
+  [YVUSDC, 0, 5000, 6], // USDC/YVUSDC is 80% LTV
+  [UNI, 0, 400, 18], // ETH/UNI is 67% LTV
+  [FRAX, 0, 400, 18], // DAI/FRAX is 91% LTV
 ]
