40e656cf44146fd0a5be9b68f0def6df61bbe605
iamsahu <praffulsahu@gmail.com>
2022-11-11 12:41:46 +0530

    fix: removed orchestration form giver deployment

--- scripts/governance/add/addUtilityContracts/giver.sh
+++ scripts/governance/add/addUtilityContracts/giver.sh
@@ -5,6 +5,3 @@ HERE=$(dirname $0)
 export CONF=$PWD/$HERE/../../base.mainnet.config
 RUN="npx hardhat run --network tenderly"
 $RUN $HERE/deployGiver.ts
-$RUN $HERE/orchestrateGiver.ts
-$RUN $HERE/../../../../shared/approve.ts
-$RUN $HERE/../../../../shared/execute.ts
