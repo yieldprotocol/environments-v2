8f4ce0de7921c42a19634457e5d0a63d5f677167
iamsahu <praffulsahu@gmail.com>
2022-11-14 15:23:39 +0530

    fix: for ROOT permission removal on giver

--- scripts/governance/add/addLever/strategyLever.config.ts
+++ scripts/governance/add/addLever/strategyLever.config.ts
@@ -6,8 +6,8 @@ import * as base_config from '../../base.arb_mainnet.config'
 import { ContractDeployment } from '../../confTypes'
 
 export const chainId: number = base_config.chainId
-export const developer: string = '0xfe90d993367bc93D171A5ED88ab460759DE2bED6'
-export const deployer: string = '0xfe90d993367bc93D171A5ED88ab460759DE2bED6'
+export const developer: string = '0x06FB6f89eAA936d4Cfe58FfA071cf8EAe17ac9AB'
+export const deployer: string = '0x06FB6f89eAA936d4Cfe58FfA071cf8EAe17ac9AB'
 export const whales: Map<string, string> = base_config.whales
 export const governance: Map<string, string> = base_config.governance
 export const protocol = () => readAddressMappingIfExists('protocol.json')
