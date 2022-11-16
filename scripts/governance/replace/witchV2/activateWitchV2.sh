9a2e7ecae2364f3cdce0022bcb84fa41961307a2
alcueca <alberto@yield.is>
2022-11-16 15:47:43 +0000

    proposal data

--- scripts/governance/replace/witchV2/activateWitchV2.sh
+++ scripts/governance/replace/witchV2/activateWitchV2.sh
@@ -3,12 +3,12 @@
 set -eux
 export HERE=$(dirname $0)
 export CONF=$PWD/$HERE/activateWitchV2.mainnet.config
-# RUN="npx hardhat run --network mainnet"
-RUN="npx hardhat run --network localhost"
+RUN="npx hardhat run --network mainnet"
+# RUN="npx hardhat run --network localhost"
 # RUN="npx hardhat run --network tenderly_witchV2"
 
 # $RUN $HERE/../../../../shared/deploy.ts
 
 # $RUN $HERE/activateWitchV2.ts
-$RUN $HERE/../../../../shared/approve.ts
+# $RUN $HERE/../../../../shared/approve.ts
 $RUN $HERE/../../../../shared/execute.ts
