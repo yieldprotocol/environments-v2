3d0642c83179bdaa11a405d57a8306a63f1b57f6
iamsahu <46891804+iamsahu@users.noreply.github.com>
2022-11-28 12:38:53 +0530

    Crab as a collateral (#268)
    * feat: crab as a collateral
    
    * feat: working tests
    
    * chore: updated limits
    
    * fix: updated proposal to account for oracle changes
    
    * fix: changes incorporated

---
+++ scripts/governance/add/addCollateral/addCrabCollateral/addCrab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+HERE=$(dirname $0)
+export CONF=$PWD/$HERE/addCrab.mainnet.config
+RUN="npx hardhat run --network tenderly"
+
+$RUN $HERE/../../../../../shared/deploy.ts
+$RUN $HERE/addCrab.ts
+$RUN $HERE/../../../../../shared/approve.ts
+$RUN $HERE/../../../../../shared/execute.ts
+$RUN $HERE/addCrab.test.ts
