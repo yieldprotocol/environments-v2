92cacb602e624c2b0a2e9a73977f5e07d6546a44
alcueca <alberto@yield.is>
2023-01-18 11:36:49 +0000

    Proposed on Arbitrum

--- scripts/governance/replace/cloakV2/replaceCloak.arbitrum.sh
+++ scripts/governance/replace/cloakV2/replaceCloak.arbitrum.sh
@@ -7,8 +7,8 @@ export CONF=$PWD/$HERE/replaceCloak.arbitrum.config
 # RUN="npx hardhat run --network tenderly"
 RUN="npx hardhat run --network arb_mainnet"
 
-$RUN $HERE/../../../../shared/deploy.ts
+# $RUN $HERE/../../../../shared/deploy.ts
 
-# $RUN $HERE/replaceCloak.arbitrum.ts
+$RUN $HERE/replaceCloak.arbitrum.ts
 # $RUN $HERE/../../../../shared/approve.ts
 # $RUN $HERE/../../../../shared/execute.ts
