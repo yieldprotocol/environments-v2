44ca2b2105388f6ba61d88044198e9c852115518
iamsahu <46891804+iamsahu@users.noreply.github.com>
2023-01-23 09:08:12 +0530

    Revoke developer permissions for Richie, David, Raja (#299)
    * feat: revokeDev
    
    * fix
    
    * The imported timelock doesn't have the *Repeated functions anymore
    
    * Proposed
    
    Co-authored-by: alcueca <alberto@yield.is>

---
+++ scripts/governance/permissions/revokeDevelopers/revokeDevelopers.config.ts
@@ -0,0 +1,11 @@
+import * as base_config from '../../base.arb_mainnet.config'
+
+export const governance: Map<string, string> = base_config.governance
+
+export const revokeDevelopers: string[] = [
+  '0x08462a2a815FE5B65D67AaC8d928C21Be2f9dDAE', // David
+  '0xa1a58E83AD7367f04501d454c7d010AB01C29d04', // Raja
+  '0xfe90d993367bc93D171A5ED88ab460759DE2bED6', // Richie
+]
+
+export const developer = '0x06FB6f89eAA936d4Cfe58FfA071cf8EAe17ac9AB'
