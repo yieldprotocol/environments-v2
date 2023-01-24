8343eb0bb78ac751645d3e503d690e94afb7bfed
alcueca <alberto@yield.is>
2023-01-24 14:47:53 +0000

    Executed on tenderly forks

--- scripts/governance/add/addUtilityContracts/addAssert/addAssert.config.ts
+++ scripts/governance/add/addUtilityContracts/addAssert/addAssert.config.ts
@@ -8,8 +8,8 @@ import { ContractDeployment } from '../../../confTypes'
 export const chainId: number = base_config.chainId
 export const governance: Map<string, string> = base_config.governance
 export const protocol = () => readAddressMappingIfExists('protocol.json')
-export const deployer: string = '0xfe90d993367bc93D171A5ED88ab460759DE2bED6'
-export const developer: string = '0xfe90d993367bc93D171A5ED88ab460759DE2bED6'
+export const deployer: string = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
+export const developer: string = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 
 // ----- deployment parameters -----
 export const contractDeployments: ContractDeployment[] = [
