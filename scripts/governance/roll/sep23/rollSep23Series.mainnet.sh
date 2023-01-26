a31a12e7a7502446ba2bff023e273e8ff97fa797
alcueca <alberto@yield.is>
2023-01-26 14:34:23 +0000

    basic roll executes

--- scripts/governance/roll/sep23/rollSep23Series.mainnet.sh
+++ scripts/governance/roll/sep23/rollSep23Series.mainnet.sh
@@ -5,15 +5,15 @@ export HERE=$(dirname $0)
 RUN="npx hardhat run --network localhost"
 
 # Phase 1: Deploy Contracts
-export CONF=$PWD/$HERE/rollSep23Series.mainnet.deployments
-$RUN $HERE/../../../../shared/deploy.ts
+# export CONF=$PWD/$HERE/rollSep23Series.mainnet.deployments
+# $RUN $HERE/../../../../shared/deploy.ts
 
 # Phase 2: Proposal
 export CONF=$PWD/$HERE/rollSep23Series.mainnet.config
 # $RUN $HERE/../../../../tools/poolRollBalances.ts
 # $RUN $HERE/../../../../tools/joinLoan.ts
 
-$RUN $HERE/../../../../tools/advanceTimeToMaturity.ts
-$RUN $HERE/../rollSeries.ts
-# $RUN $HERE/../../../../shared/approve.ts
-# $RUN $HERE/../../../../shared/execute.ts
+# $RUN $HERE/../../../../tools/advanceTimeToMaturity.ts
+# $RUN $HERE/../rollSeries.ts
+$RUN $HERE/../../../../shared/approve.ts
+$RUN $HERE/../../../../shared/execute.ts
