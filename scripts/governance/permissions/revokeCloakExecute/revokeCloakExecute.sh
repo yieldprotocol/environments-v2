9921ba46da6a092be045bc1a3dd1d9df9accec0d
alcueca <alberto@yield.is>
2023-01-27 10:11:23 +0000

    add revokeCloakExecute scripts

---
+++ scripts/governance/permissions/revokeCloakExecute/revokeCloakExecute.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+export HERE=$(dirname $0)
+export CONF=$PWD/$HERE/revokeCloakExecute.config
+RUN="npx hardhat run --network arb_mainnet"
+# RUN="npx hardhat run --network tenderly"
+
+$RUN $HERE/revokeCloakExecute.ts
+# $RUN $HERE/../../../../shared/approve.ts
+# $RUN $HERE/../../../../shared/execute.ts
