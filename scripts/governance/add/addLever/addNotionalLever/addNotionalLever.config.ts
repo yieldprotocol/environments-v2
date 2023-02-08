a500fd1ee8a5b4b2042abfe8c7fcee1fd91c9ac4
iamsahu <praffulsahu@gmail.com>
2023-02-08 19:09:55 +0530

    feat: updated flashfee

--- scripts/governance/add/addLever/addNotionalLever/addNotionalLever.config.ts
+++ scripts/governance/add/addLever/addNotionalLever/addNotionalLever.config.ts
@@ -32,24 +32,3 @@ export const contractDeployments: ContractDeployment[] = [
     args: [() => protocol().getOrThrow(GIVER)],
   },
 ]
-
-/// @notice Flashfeefactor to be set on asset's join
-/// @param assetId
-/// @param flashFeeAmount
-export const joinFlashFees: [string, string][] = [
-  ['0x303000000000', '0'],
-  ['0x303100000000', '0'],
-  ['0x303200000000', '0'],
-]
-
-/// @notice Flashfeefactor to be set on fyTokens
-/// @param seriesId
-/// @param flashFee
-export const fyTokenFlashFees: [string, string][] = [
-  [FYETH2303, '0'], //FYETH2303
-  [FYDAI2303, '0'], //FYDAI2303
-  [FYUSDC2303, '0'], //FYUSDC2303
-  [FYETH2306, '0'], //FYETH2306
-  [FYDAI2306, '0'], //FYDAI2306
-  [FYUSDC2306, '0'], //FYUSDC2306
-]
--- scripts/governance/update/updateFlashFees/updateFlashFees.sh
+++ scripts/governance/update/updateFlashFees/updateFlashFees.sh
@@ -3,11 +3,9 @@
 
 set -eux
 HERE=$(dirname $0)
-export CONF=$PWD/$HERE/strategyLever.config.ts
+export CONF=$PWD/$HERE/updateFlashFees.config.ts
 RUN="npx hardhat run --network tenderly"
 
-$RUN $HERE/../../../../shared/deploy.ts
-$RUN $HERE/strategyLeverAndGiverSetup.ts
+$RUN $HERE/updateFlashFees.ts
 $RUN $HERE/../../../../shared/approve.ts
 $RUN $HERE/../../../../shared/execute.ts
-$RUN $HERE/stratLever.test.ts
