e59e64c91db312c13bd5424055e6d72de2f7e6ae
alcueca <alberto@yield.is>
2023-02-08 12:31:12 +0000

    arbitrum and mainnet execute on localhost

--- scripts/governance/replace/cloakV2/contango/replaceCloak.mainnet.config.ts
+++ scripts/governance/replace/cloakV2/contango/replaceCloak.mainnet.config.ts
@@ -1,3 +1,5 @@
+import { readAddressMappingIfExists } from '../../../../../shared/helpers'
+
 import { CONTANGO_WITCH, CONTANGO_LADLE } from '../../../../../shared/constants'
 
 import * as base_config from '../../../base.mainnet.config'
@@ -7,9 +9,7 @@ export const developer: string = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 export const governance: Map<string, string> = base_config.governance
 export const protocol: Map<string, string> = base_config.protocol
 export const assets: Map<string, string> = base_config.assets
-export const joins: Map<string, string> = base_config.joins
-export const fyTokens: Map<string, string> = base_config.fyTokens
-export const pools: Map<string, string> = base_config.pools
-export const external: Map<string, string> = base_config.external
+export const joins: Map<string, string> = readAddressMappingIfExists('contangoJoins.json')
+export const fyTokens: Map<string, string> = readAddressMappingIfExists('contangoFYTokens.json')
 
 export const users: Array<string> = [protocol.getOrThrow(CONTANGO_LADLE), protocol.getOrThrow(CONTANGO_WITCH)]
--- scripts/governance/replace/cloakV2/contango/replaceCloak.sh
+++ scripts/governance/replace/cloakV2/contango/replaceCloak.sh
@@ -2,7 +2,7 @@
 
 set -eux
 export HERE=$(dirname $0)
-export CONF=$PWD/$HERE/replaceCloak.arbitrum.config
+export CONF=$PWD/$HERE/replaceCloak.mainnet.config
 RUN="npx hardhat run --network localhost"
 # RUN="npx hardhat run --network tenderly"
 
