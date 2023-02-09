3847a9d60f604cb9cddda0b21e853c178e169d4d
brucedonovan <bruce.donovan@gmail.com>
2023-02-09 16:30:48 +0000

    updateLimit config

--- scripts/governance/update/updateLimits/updateLimits.mainnet.config.ts
+++ scripts/governance/update/updateLimits/updateLimits.mainnet.config.ts
@@ -1,4 +1,19 @@
-import { ETH, DAI, USDC, WBTC, WSTETH, LINK, ENS, UNI, YVUSDC, FRAX } from '../../../../shared/constants'
+import {
+  ETH,
+  DAI,
+  USDC,
+  WBTC,
+  WSTETH,
+  LINK,
+  ENS,
+  UNI,
+  YVUSDC,
+  FRAX,
+  FDAI2303,
+  FDAI2306,
+  FETH2303,
+  FETH2306,
+} from '../../../../shared/constants'
 
 import * as base_config from '../../base.mainnet.config'
 
@@ -17,14 +32,10 @@ export const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 /// @param Minimum vault debt allowed for this pair, modified by decimals
 /// @param Decimals to append to global and vault debt.
 export const newLimits: Array<[string, string, number, number, number]> = [
-  [ETH, ETH, 2500, 0, 18],
-  [ETH, DAI, 250, 3, 18],
-  [ETH, USDC, 250, 3, 18],
-  [ETH, WBTC, 250, 3, 18],
-  [ETH, WSTETH, 250, 3, 18],
-  [ETH, LINK, 250, 3, 18],
-  [ETH, ENS, 250, 3, 18],
-  [ETH, YVUSDC, 0, 0, 18],
-  [ETH, UNI, 250, 3, 18],
-  [ETH, FRAX, 250, 3, 18],
+  // [ETH, FETH2303, 400, 1, 18],
+  // [ETH, FETH2306, 400, 1, 18],
+  [DAI, FDAI2303, 1000000, 1000, 18],
+  [DAI, FDAI2306, 1000000, 1000, 18],
+  [USDC, FDAI2303, 1000000, 1000, 6],
+  [USDC, FDAI2306, 1000000, 1000, 6],
 ]
