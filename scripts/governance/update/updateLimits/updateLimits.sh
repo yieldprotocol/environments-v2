bbfe732a9e145d7e19f011a6eb3c0be6810b6dea
brucedonovan <bruce.donovan@gmail.com>
2023-02-10 11:41:34 +0000

    start add in approve/execute

--- scripts/governance/update/updateLimits/updateLimits.sh
+++ scripts/governance/update/updateLimits/updateLimits.sh
@@ -5,11 +5,12 @@ HERE=$(dirname $0)
 export CONF=$PWD/$HERE/updateLimits.mainnet.config
 RUN="npx hardhat run --network tenderly"
 
-# Action: UPDATE the limits
+# Action: UPDATE the limits, poropose, apporve, execute and test
 $RUN $HERE/updateLimits.ts
+
+$RUN ./shared/approve.ts
+$RUN ./shared/execute.ts
+$RUN $HERE/updateLimits.test.ts 
 
 # Or :  DISPLAY the current limits
 # $RUN $HERE/displayLimits.ts
-
-# Or :  TEST the update
-# $RUN $HERE/updateLimits.test.ts 
