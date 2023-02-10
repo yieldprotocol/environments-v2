c1edfb5113aa3ba8fddf9c453c5b0976a76d30b9
brucedonovan <bruce.donovan@gmail.com>
2023-02-10 15:42:25 +0000

    updateLimits using ilk only

--- scripts/governance/update/updateLimits/updateLimits.mainnet.config.ts
+++ scripts/governance/update/updateLimits/updateLimits.mainnet.config.ts
@@ -18,6 +18,7 @@ import {
 } from '../../../../shared/constants'
 
 import * as base_config from '../../base.mainnet.config'
+import { Ilk } from '../../confTypes'
 
 export const whales: Map<string, string> = base_config.whales
 export const governance: Map<string, string> = base_config.governance
@@ -28,14 +29,10 @@ export const joins: Map<string, string> = base_config.joins
 export const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
 
 /// @notice Limits to be used in an auction
-/// @param base identifier (bytes6 tag)
 /// @param ilk identifier (bytes6 tag)
-/// @param Maximum global debt allowed for this pair, modified by decimals
-/// @param Minimum vault debt allowed for this pair, modified by decimals
-/// @param Decimals to append to global and vault debt.
-export const newLimits: Array<[string, string, number, number, number]> = [
-  [DAI, FDAI2303, 1000000, 1000, 18],
-  [DAI, FDAI2306, 1000000, 1000, 18],
-  [USDC, FUSDC2303, 1000000, 1000, 6],
-  [USDC, FUSDC2306, 1000000, 1000, 6],
+export const newLimits: Ilk[] = [
+  base_config.ilkUSDCFUSDC2303,
+  base_config.ilkUSDCFUSDC2306,
+  base_config.ilkDAIFDAI2303,
+  base_config.ilkDAIFDAI2306,
 ]
