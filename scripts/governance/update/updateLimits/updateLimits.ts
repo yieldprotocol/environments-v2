b3a1edf99d8f30feaae74e48168d37d3e6444c99
Bruce Donovan <brucedonovan@users.noreply.github.com>
2023-02-10 13:01:42 +0000

    Update scripts/governance/update/updateLimits/updateLimits.ts
    Co-authored-by: Alberto Cuesta Cañada <38806121+alcueca@users.noreply.github.com>

--- scripts/governance/update/updateLimits/updateLimits.ts
+++ scripts/governance/update/updateLimits/updateLimits.ts
@@ -44,5 +44,5 @@ const { governance, protocol, developer, newLimits } = require(process.env.CONF 
   ).flat()
 
   // Propose, Approve & execute
-  await propose(timelock, proposal, governance.get('multisig') as string)
+  await propose(timelock, proposal, governance.get(MULTISIG) as string)
 })()
