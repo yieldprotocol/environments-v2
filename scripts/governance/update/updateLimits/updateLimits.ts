8a171d51026b2dd48931c51cc10b5bcb33ba6c2c
brucedonovan <bruce.donovan@gmail.com>
2023-02-10 15:51:20 +0000

    handle Ilks

--- scripts/governance/update/updateLimits/updateLimits.ts
+++ scripts/governance/update/updateLimits/updateLimits.ts
@@ -20,21 +20,13 @@ const { governance, protocol, developer, newLimits } = require(process.env.CONF 
   const timelock = Timelock__factory.connect(governance.getOrThrow(TIMELOCK)!, ownerAcc)
 
   // Build the proposal
-
   /** This is the original code **/
   // let proposal: Array<{ target: string; data: string }> = []
   // proposal = proposal.concat(await updateDebtLimits(cauldron, newLimits))
 
   /* New code to handle Ilk type as updateDebtLimits() function argument */
-  const proposal = (
-    await Promise.all(
-      newLimits.map((newLimit: [string, string, number, number, number]) => {
-        const [baseId, ilkId, line, dust, dec] = newLimit
-        return updateDebtLimits(cauldron, { baseId, ilkId, debtLimits: { baseId, ilkId, line, dust, dec } } as Ilk) // Casting to Ilk type
-      })
-    )
-  ).flat()
+  const proposal = await Promise.all(newLimits.map((ilk: Ilk) => updateDebtLimits(cauldron, ilk)))
 
   // Propose, Approve & execute
-  await propose(timelock, proposal, governance.get(MULTISIG) as string)
+  await propose(timelock, proposal.flat(), governance.get(MULTISIG) as string)
 })()
