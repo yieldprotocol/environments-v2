a0ff567666aa95a1946b2130fb0560f71cc96bcc
alcueca <alberto@yield.is>
2023-03-06 10:42:14 +0000

    executed

--- scripts/governance/update/updateDebtLimits/updateLimits.sh
+++ scripts/governance/update/updateDebtLimits/updateLimits.sh
@@ -2,14 +2,14 @@
 
 set -eux
 export HERE=$(dirname $0)
-export CONF=$PWD/$HERE/updateLimits.arb_mainnet.config
+export CONF=$PWD/$HERE/yearnLimits.config
 # RUN="npx hardhat run --network arb_mainnet"
-RUN="npx hardhat run --network tenderly"
+RUN="npx hardhat run --network mainnet"
 
 # Action: UPDATE the limits, propose, apporve, execute and test
 # $RUN $HERE/updateLimits.ts
 
-$RUN $HERE/../../../../shared/approve.ts
+# $RUN $HERE/../../../../shared/approve.ts
 $RUN $HERE/../../../../shared/execute.ts
 # $RUN $HERE/updateLimits.test.ts 
 
