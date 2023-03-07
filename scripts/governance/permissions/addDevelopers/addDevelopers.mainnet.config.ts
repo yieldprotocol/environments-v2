5355362c882bc4d47e67db00f9ca6d3e2c036854
iamsahu <praffulsahu@gmail.com>
2023-03-07 14:57:52 +0530

    feat: proposal

--- scripts/governance/permissions/addDevelopers/addDevelopers.mainnet.config.ts
+++ scripts/governance/permissions/addDevelopers/addDevelopers.mainnet.config.ts
@@ -3,7 +3,7 @@ import * as base_config from '../../base.mainnet.config'
 export const governance: Map<string, string> = base_config.governance
 
 export const newDevelopers: string[] = [
-  '0x05950b4e68f103d5aBEf20364dE219a247e59C23', // Bruno
+  '0x1Bd3Abb6ef058408734EA01cA81D325039cd7bcA', // Bruce
 ]
 
 export const developer = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
