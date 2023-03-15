d524b41b987cb5f64bd250d66b1587ff4375396b
alcueca <alberto@yield.is>
2023-03-15 11:15:21 +0000

    feat: restore September series and MMS strategies

---
+++ scripts/governance/emergency/euler/restoreSepStrategies.config.ts
@@ -0,0 +1,130 @@
+import { ethers } from 'hardhat'
+import { ETH, DAI, USDC, USDT } from '../../../../shared/constants'
+import { ACCUMULATOR } from '../../../../shared/constants'
+import { FYETH2309, FYDAI2309, FYUSDC2309, FYUSDT2309 } from '../../../../shared/constants'
+import { YSETH6MMS, YSDAI6MMS, YSUSDC6MMS, YSUSDT6MMS } from '../../../../shared/constants'
+
+import * as base_config from '../../base.mainnet.config'
+
+export const chainId: number = base_config.chainId
+export const developer: string = '0xC7aE076086623ecEA2450e364C838916a043F9a8'
+export const deployers: Map<string, string> = base_config.deployers
+export const whales: Map<string, string> = base_config.whales
+
+export const governance: Map<string, string> = base_config.governance
+export const protocol: Map<string, string> = base_config.protocol
+export const assets: Map<string, string> = base_config.assets
+export const joins: Map<string, string> = base_config.joins
+export const fyTokens: Map<string, string> = base_config.fyTokens
+export const pools: Map<string, string> = base_config.pools
+export const strategyAddresses: Map<string, string> = base_config.strategyAddresses
+
+export const series: Map<string, Series> = base_config.series
+export const strategies: Map<string, Strategy> = base_config.strategies
+
+import { Series, Strategy, Strategy_V1 } from '../../confTypes'
+
+const eth = base_config.bases.getOrThrow(ETH)!
+const dai = base_config.bases.getOrThrow(DAI)!
+const usdc = base_config.bases.getOrThrow(USDC)!
+const frax = base_config.bases.getOrThrow(USDT)!
+const ethIlks = base_config.ilks.getOrThrow(ETH)!
+const daiIlks = base_config.ilks.getOrThrow(DAI)!
+const usdcIlks = base_config.ilks.getOrThrow(USDC)!
+const fraxIlks = base_config.ilks.getOrThrow(USDT)!
+
+const fyETH2309: Series = {
+  seriesId: FYETH2309,
+  base: eth,
+  fyToken: {
+    assetId: FYETH2309,
+    address: fyTokens.getOrThrow(FYETH2309)!,
+  },
+  chiOracle: protocol.getOrThrow(ACCUMULATOR)!,
+  pool: {
+    assetId: FYETH2309,
+    address: pools.getOrThrow(FYETH2309)!,
+  },
+  ilks: ethIlks,
+}
+
+const fyDAI2309: Series = {
+  seriesId: FYDAI2309,
+  base: dai,
+  fyToken: {
+    assetId: FYDAI2309,
+    address: fyTokens.getOrThrow(FYDAI2309)!,
+  },
+  chiOracle: protocol.getOrThrow(ACCUMULATOR)!,
+  pool: {
+    assetId: FYDAI2309,
+    address: pools.getOrThrow(FYDAI2309)!,
+  },
+  ilks: daiIlks,
+}
+
+const fyUSDC2309: Series = {
+  seriesId: FYUSDC2309,
+  base: usdc,
+  fyToken: {
+    assetId: FYUSDC2309,
+    address: fyTokens.getOrThrow(FYUSDC2309)!,
+  },
+  chiOracle: protocol.getOrThrow(ACCUMULATOR)!,
+  pool: {
+    assetId: FYUSDC2309,
+    address: pools.getOrThrow(FYUSDC2309)!,
+  },
+  ilks: usdcIlks,
+}
+
+const fyUSDT2309: Series = {
+  seriesId: FYUSDT2309,
+  base: frax,
+  fyToken: {
+    assetId: FYUSDT2309,
+    address: fyTokens.getOrThrow(FYUSDT2309)!,
+  },
+  chiOracle: protocol.getOrThrow(ACCUMULATOR)!,
+  pool: {
+    assetId: FYUSDT2309,
+    address: pools.getOrThrow(FYUSDT2309)!,
+  },
+  ilks: fraxIlks,
+}
+
+export const newSeries: Series[] = [fyETH2309, fyDAI2309, fyUSDC2309, fyUSDT2309]
+
+const ysETH6MMS: Strategy = {
+  assetId: YSETH6MMS,
+  address: strategyAddresses.getOrThrow(YSETH6MMS)!,
+  base: eth,
+  seriesToInvest: fyETH2309,
+  initAmount: ethers.utils.parseUnits('1000', 18),
+}
+
+const ysDAI6MMS: Strategy = {
+  assetId: YSDAI6MMS,
+  address: strategyAddresses.getOrThrow(YSDAI6MMS)!,
+  base: dai,
+  seriesToInvest: fyDAI2309,
+  initAmount: ethers.utils.parseUnits('1000000', 18),
+}
+
+const ysUSDC6MMS: Strategy = {
+  assetId: YSUSDC6MMS,
+  address: strategyAddresses.getOrThrow(YSUSDC6MMS)!,
+  base: usdc,
+  seriesToInvest: fyUSDC2309,
+  initAmount: ethers.utils.parseUnits('1000000', 6),
+}
+
+const ysUSDT6MMS: Strategy = {
+  assetId: YSUSDT6MMS,
+  address: strategyAddresses.getOrThrow(YSUSDT6MMS)!,
+  base: frax,
+  seriesToInvest: fyUSDT2309,
+  initAmount: ethers.utils.parseUnits('1000000', 6),
+}
+
+export const newStrategies: Strategy[] = [ysETH6MMS, ysDAI6MMS, ysUSDC6MMS, ysUSDT6MMS]
