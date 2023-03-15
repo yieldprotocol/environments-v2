29c366548ded98bae06fa491ad1a9e0c240a028e
alcueca <alberto@yield.is>
2023-03-15 10:59:58 +0000

    feat: restore June series

---
+++ scripts/governance/emergency/euler/restoreJunStrategies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+export HERE=$(dirname $0)
+RUN="npx hardhat run --network tenderly"
+
+# Phase 1: Deploy Contracts
+export CONF=$PWD/$HERE/restoreJunStrategies.deployments
+$RUN $HERE/../../../../shared/deploy.ts
+
+# Phase 2: Proposal
+export CONF=$PWD/$HERE/restoreJunStrategies.config
+# $RUN $HERE/../../../../tools/loadTimelock.ts
+
+# $RUN $HERE/../../../../tools/advanceTimeToMaturity.ts
+$RUN $HERE/restoreJunStrategies.ts
+$RUN $HERE/../../../../shared/approve.ts
+$RUN $HERE/../../../../shared/execute.ts
