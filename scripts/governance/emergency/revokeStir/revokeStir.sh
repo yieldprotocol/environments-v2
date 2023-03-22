85c3efd9224c3b2554e0d88fa3383302e609d3f4
alcueca <alberto@yield.is>
2023-03-22 14:30:53 +0000

    fix: consider cloak

--- scripts/governance/emergency/revokeStir/revokeStir.sh
+++ scripts/governance/emergency/revokeStir/revokeStir.sh
@@ -3,8 +3,8 @@
 set -eux
 export HERE=$(dirname $0)
 export CONF=$PWD/$HERE/../../base.mainnet.config
-RUN="npx hardhat run --network mainnet"
+RUN="npx hardhat run --network tenderly"
 
 $RUN $HERE/revokeStir.ts
-# $RUN $HERE/../../../../shared/approve.ts
-# $RUN $HERE/../../../../shared/execute.ts
+$RUN $HERE/../../../../shared/approve.ts
+$RUN $HERE/../../../../shared/execute.ts
--- scripts/governance/emergency/revokeStir/revokeStir.ts
+++ scripts/governance/emergency/revokeStir/revokeStir.ts
@@ -1,7 +1,8 @@
 import { getOwnerOrImpersonate, propose } from '../../../../shared/helpers'
-import { Timelock__factory, Cauldron__factory } from '../../../../typechain'
-import { TIMELOCK, CLOAK, CAULDRON, LADLE } from '../../../../shared/constants'
+import { Timelock__factory, Cauldron__factory, Assert__factory } from '../../../../typechain'
+import { TIMELOCK, CLOAK, CAULDRON, LADLE, ASSERT } from '../../../../shared/constants'
 import { revokeOrchestration } from '../../../fragments/permissions/revokeOrchestration'
+import { checkPlan } from '../../../fragments/cloak/checkPlan'
 import { id } from '../../../../shared/helpers'
 import { EmergencyBrake__factory } from '../../../../typechain/factories/@yield-protocol/utils-v2/contracts/utils'
 
@@ -16,6 +17,7 @@ const { developer, governance, protocol } = require(process.env.CONF as string)
   const timelock = Timelock__factory.connect(governance.get(TIMELOCK)!, ownerAcc)
   const cloak = EmergencyBrake__factory.connect(governance.get(CLOAK)!, ownerAcc)
   const cauldron = Cauldron__factory.connect(protocol.get(CAULDRON)!, ownerAcc)
+  const assert = Assert__factory.connect(protocol.get(ASSERT)!, ownerAcc)
 
   let proposal: Array<{ target: string; data: string }> = []
   proposal = proposal.concat(
@@ -26,6 +28,8 @@ const { developer, governance, protocol } = require(process.env.CONF as string)
       id(cauldron.interface, 'stir(bytes12,bytes12,uint128,uint128)')
     )
   )
+
+  proposal = proposal.concat(await checkPlan(cloak, assert, [protocol.getOrThrow(LADLE)!]))
 
   await propose(timelock, proposal, developer)
 })()
