5653308912663c29ece4a6327374b66fad655372
alcueca <alberto@yield.is>
2023-03-28 16:59:31 +0100

    added tokenSwap to the ladle as an integration

--- scripts/governance/emergency/euler/sepStrategyTokenSwap.ts
+++ scripts/governance/emergency/euler/sepStrategyTokenSwap.ts
@@ -2,15 +2,17 @@ import { getOwnerOrImpersonate, propose } from '../../../../shared/helpers'
 
 import {
   Timelock__factory,
+  Ladle__factory,
   ERC20__factory,
   TokenSwap__factory,
 } from '../../../../typechain'
 
-import { TIMELOCK, TOKEN_SWAP } from '../../../../shared/constants'
+import { TIMELOCK, LADLE, TOKEN_SWAP } from '../../../../shared/constants'
 
 import { orchestrateTokenSwap } from '../../../fragments/utils/orchestrateTokenSwap'
 import { registerSwap } from '../../../fragments/utils/registerSwap'
 import { sendTokens } from '../../../fragments/utils/sendTokens'
+import { addIntegration } from '../../../fragments/ladle/addIntegration'
 
 import { Asset, Transfer } from '../../confTypes'
 
@@ -24,54 +26,63 @@ const { developer, deployers, governance, protocol, strategies, swaps } = requir
   let ownerAcc = await getOwnerOrImpersonate(developer)
 
   const timelock = Timelock__factory.connect(governance.getOrThrow(TIMELOCK)!, ownerAcc)
+  const ladle = Ladle__factory.connect(protocol.getOrThrow(LADLE)!, ownerAcc)
   const tokenSwap = TokenSwap__factory.connect(protocol.getOrThrow(TOKEN_SWAP)!, ownerAcc)
 
   // Build the proposal
   let proposal: Array<{ target: string; data: string }> = []
 
-  // Orchestrate tokenSwap
+//  // Orchestrate tokenSwap
+//  proposal = proposal.concat(
+//    await orchestrateTokenSwap(
+//      deployers.getOrThrow(tokenSwap.address),
+//      tokenSwap,
+//      timelock,
+//    )
+//  )
+
+  // Add tokenSwap to ladle as an integration
   proposal = proposal.concat(
-    await orchestrateTokenSwap(
-      deployers.getOrThrow(tokenSwap.address),
-      tokenSwap,
-      timelock,
+    await addIntegration(
+      ladle,
+      tokenSwap.address
     )
   )
 
-  // Register strategy token swaps
-  for (let swap of swaps) {
-    const tokenOutAddress = strategies.getOrThrow(swap.tokenOut)
-    const tokenOut = ERC20__factory.connect(tokenOutAddress, ownerAcc)
-    const tokenInAddress = strategies.getOrThrow(swap.tokenIn)
-
-    const transferAsset: Asset = {
-      assetId: swap.tokenOut,
-      address: tokenOutAddress,
-    }
-
-    const transferAmount = await tokenOut.balanceOf(timelock.address)
-
-    const transfer: Transfer = {
-      token: transferAsset,
-      receiver: tokenSwap.address,
-      amount: transferAmount
-    }
-
-    proposal = proposal.concat(
-      await sendTokens(
-        timelock,
-        transfer,
-      )
-    )
-
-    proposal = proposal.concat(
-      await registerSwap(
-        tokenSwap,
-        tokenInAddress,
-        tokenOutAddress,
-      )
-    )
-  }
+//  // Register strategy token swaps
+//  for (let swap of swaps) {
+//    const tokenOutAddress = strategies.getOrThrow(swap.tokenOut)
+//    const tokenOut = ERC20__factory.connect(tokenOutAddress, ownerAcc)
+//    const tokenInAddress = strategies.getOrThrow(swap.tokenIn)
+//
+//    const transferAsset: Asset = {
+//      assetId: swap.tokenOut,
+//      address: tokenOutAddress,
+//    }
+//
+//    const transferAmount = await tokenOut.balanceOf(timelock.address)
+//
+//    const transfer: Transfer = {
+//      token: transferAsset,
+//      receiver: tokenSwap.address,
+//      amount: transferAmount
+//    }
+//
+//    proposal = proposal.concat(
+//      await sendTokens(
+//        timelock,
+//        transfer,
+//      )
+//    )
+//
+//    proposal = proposal.concat(
+//      await registerSwap(
+//        tokenSwap,
+//        tokenInAddress,
+//        tokenOutAddress,
+//      )
+//    )
+//  }
 
   await propose(timelock, proposal, developer)
 })()
