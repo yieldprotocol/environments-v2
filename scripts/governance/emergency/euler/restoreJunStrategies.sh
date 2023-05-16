d0c80f3e7ed1ee6520e40f0144cb273aff5f3711
alcueca <alberto@yield.is>
2023-05-16 11:31:31 +0100

    feat: restored protocol on mainnet

--- scripts/governance/emergency/euler/restoreJunStrategies.sh
+++ scripts/governance/emergency/euler/restoreJunStrategies.sh
@@ -2,7 +2,7 @@
 
 set -eux
 export HERE=$(dirname $0)
-RUN="npx hardhat run --network tenderly"
+RUN="npx hardhat run --network mainnet"
 
 # Phase 1: Deploy Contracts
 # export CONF=$PWD/$HERE/restoreJunStrategies.deployments
