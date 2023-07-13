dd33edf53da8e3765fc3c7b4685e6e97f34cf23d
alcueca <alberto@yield.is>
2023-07-13 14:45:37 +0100

    all proposed

--- scripts/governance/roll/fundTrader/fundTrader.sh
+++ scripts/governance/roll/fundTrader/fundTrader.sh
@@ -2,11 +2,11 @@
 
 set -eux
 export HERE=$(dirname $0)
-RUN="npx hardhat run --network tenderly"
+RUN="npx hardhat run --network mainnet"
 
 
 export CONF=$PWD/$HERE/fundTrader.mainnet.config
 
 $RUN $HERE/fundTrader.ts
-$RUN $HERE/../../../../shared/approve.ts
-$RUN $HERE/../../../../shared/execute.ts
+# $RUN $HERE/../../../../shared/approve.ts
+# $RUN $HERE/../../../../shared/execute.ts
--- scripts/governance/roll/sellFYToken/sellFYToken.mainnet.sh
+++ scripts/governance/roll/sellFYToken/sellFYToken.mainnet.sh
@@ -2,11 +2,11 @@
 
 set -eux
 export HERE=$(dirname $0)
-RUN="npx hardhat run --network tenderly"
+RUN="npx hardhat run --network mainnet"
 
 
 export CONF=$PWD/$HERE/sellFYToken.mainnet.config
 
 $RUN $HERE/sellFYToken.ts
-$RUN $HERE/../../../../shared/approve.ts
-$RUN $HERE/../../../../shared/execute.ts
+# $RUN $HERE/../../../../shared/approve.ts
+# $RUN $HERE/../../../../shared/execute.ts
